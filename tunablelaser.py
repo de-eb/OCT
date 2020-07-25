612282a0c5f98c12f4b92dbe14a9452ebf68329f
takuya <tkykwsm6ch@gmail.com>
2020-07-25 18:39:17 +0900

    add command list

--- tunablelaser.py
+++ tunablelaser.py
@@ -1,5 +1,17 @@
 import pyvisa
 
+command = [
+    '*IDN?',
+    ':LOCK ON|OFF,0000',
+    ':OUTP ON|OFF',  # laser output
+    ':POW:UNIT DBM|DBMW|W'  # power unit
+    ':POW:ATT:AUTO ON|OFF',  # attenuation mode
+    ':POW:ATT:DARK ON|OFF',
+    ':POW:ATT <val>DB|DEF|MIN|MAX',  # attenuation level[dBm]
+    ':POW <val>DB|DEF|MIN|MAX',  # laser output power[dBm]
+    ':WAVE <val>',  # wavelength[m]
+    ]
+
 if __name__ == "__main__":
 
     rm = pyvisa.ResourceManager()
