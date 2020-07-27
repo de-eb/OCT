3bd9d857e5c95b5545740072934b0f79a903cbff
de-eb <kawashima.takuya.bl@tut.jp>
2020-07-27 16:39:18 +0900

    [update] communication test success!

--- tunablelaser.py
+++ tunablelaser.py
@@ -16,8 +16,10 @@ if __name__ == "__main__":
 
     rm = pyvisa.ResourceManager()
     print(rm.list_resources())
-    laser = rm.open_resource('GPIB0::14::INSTR')
+    laser = rm.open_resource('GPIB0::24::INSTR')
     laser.read_termination = '\n'
     laser.write_termination = '\n'
 
     print(laser.query('*IDN?'))
+    laser.write(':LOCK OFF,0000')
+    print(laser.query(':LOCK?'))
