c4c2ce2673e3d78c8bf71184340a137103814212
takuya <tkykwsm6ch@gmail.com>
2020-07-30 22:06:06 +0900

    [update] Add functions

--- ncm6212c.py
+++ ncm6212c.py
@@ -17,114 +17,117 @@ class NCM6212C:
             baudrate = self.__baudrate,
             timeout = 0.1,
             rtscts = self.__rtscts)
-        time.sleep(0.1)
-        print(self.read_communication_setting)
-        print(self.read_firmware_version)
-        print(self.read_axis_name)
+        # time.sleep(0.1)
+        print(self.read_hardware_info)
     
-    def __command(self, cmd: str):
+    def __send(self, cmd: str):
         """ Format the command string and send it to the controller.
         """
         self.__ser.write((cmd+self.__delimiter).encode('utf-8'))
     
+    def __receive(self):
+        """ Receive a reply from the controller.
+        """
+        ret = self.__ser.read_until(self.__delimiter.encode('utf-8'))
+        return ret
+    
     def move_stage(self, axis: str, position: int):
         """ Move stage to the absolute position.
         """
-        self.__command('MV {}{}'.format(axis, position))
+        self.__send('MV {}{}'.format(axis, position))
+        # self.__send('MV? {}{}'.format(axis, position))
     
     def read_position(self, axis: str) -> str:
         """ Return the reading value (present position) from A/D converter
             of the displacement sensor by nanometer unit.
         """
-        self.__command('PS? {}'.format(axis))
-        time.sleep(0.1)
-        return self.__ser.readline()
+        self.__send('PS? {}'.format(axis))
+        # time.sleep(0.1)
+        position = self.__receive()
+        return position
     
     def set_servo_mode(self, axis: str, mode: int):
         """ Set the servo mode. There is a delay of several seconds
             when switching the servo.
         """
-        self.__command('SV {}{}'.format(axis, mode))
-
-    def set_communication(
-            self, baudrate: int, rtscts: bool, delimiter: str) -> None:
-        """ Execute setting of RS-232C communication.
+        self.__send('SV {}{}'.format(axis, mode))
+    
+    def read_hardware_info(self) -> tuple:
+        """ Returns the internal information data of the controller.
         """
-        if baudrate not in [9600, 19200, 38400, 57600, 115200]:
-            msg = "Baudrate must be 9600, 19200, 38400, 57600 or 115200."
-            raise InvalidSettingError(msg)
+        self.__send('VR?')
+        firmware_version = self.__receive()
+        self.__send('CH?')
+        axis_name = self.__receive()
+        self.__send('BD?')
+        communication_setting = self.__receive()
+        return firmware_version, axis_name, communication_setting
+    
+    def read_error(self, axis: str):
+        self.__send('ER? {}'.format(axis))
+        err = self.__receive()
+        return err
 
-        if delimiter == '\r\n':
-            dm = 0
-        elif delimiter == '\r':
-            dm = 1
-        elif delimiter == '\n':
-            dm = 2
-        else:
-            msg = r"Delimiter must be \r\n(CRLF) or \r(CR) or \n(LF)."
-            raise InvalidSettingError(msg)
+    # def set_communication(
+    #         self, baudrate: int, rtscts: bool, delimiter: str) -> None:
+    #     """ Execute setting of RS-232C communication.
+    #     """
+    #     if baudrate not in [9600, 19200, 38400, 57600, 115200]:
+    #         msg = "Baudrate must be 9600, 19200, 38400, 57600 or 115200."
+    #         raise InvalidSettingError(msg)
 
-        self.__command('BD {:.1f} {} {}'.format(baudrate/1000, int(rtscts), dm))
-        time.sleep(0.1)
-        self.__command('WPA')
+    #     if delimiter == '\r\n':
+    #         dm = 0
+    #     elif delimiter == '\r':
+    #         dm = 1
+    #     elif delimiter == '\n':
+    #         dm = 2
+    #     else:
+    #         msg = r"Delimiter must be \r\n(CRLF) or \r(CR) or \n(LF)."
+    #         raise InvalidSettingError(msg)
+
+    #     self.__send('BD {:.1f} {} {}'.format(baudrate/1000, int(rtscts), dm))
+    #     # time.sleep(0.1)
+    #     self.__send('WPA')
         
-        self.__baudrate = baudrate
-        self.__rtscts = rtscts
-        self.__delimiter = delimiter
-        print("Configuration has been updated. Reboot the piezo controller.")
-
-    def read_communication_setting(self) -> str:
-        """ Read out the communication settings of RS-232C.
-        """
-        self.__command('BD?')
-        time.sleep(0.1)
-        return self.__ser.readline()
-
-    def read_firmware_version(self) -> str:
-        """ Read out the firmware version.
-        """
-        self.__command('VR?')
-        time.sleep(0.1)
-        return self.__ser.readline()
+    #     self.__baudrate = baudrate
+    #     self.__rtscts = rtscts
+    #     self.__delimiter = delimiter
+    #     print("Configuration has been updated. Reboot the piezo controller.")
     
-    def read_axis_name(self) -> str:
-        self.__command('CH?')
-        time.sleep(0.1)
-        return self.__ser.readline()
-    
-    def reset_system(self) -> None:
-        """ Reset each setting value to the default value.
-        """
-        self.__command('RS')
-        print("All settings have been reset. Reboot the piezo controller.")
+    # def reset_system(self) -> None:
+    #     """ Reset each setting value to the default value.
+    #     """
+    #     self.__send('RS')
+    #     print("All settings have been reset. Reboot the piezo controller.")
 
-class ErrorNCM6212C(Exception):
-    """Base exception class for PiezoController.
+# class ErrorNCM6212C(Exception):
+#     """Base exception class for PiezoController.
     
-    All exceptions thrown from the package inherit this.
-    Attributes
-    ----------
-    msg : `str`
-        Human readable string describing the exception.
+#     All exceptions thrown from the package inherit this.
+#     Attributes
+#     ----------
+#     msg : `str`
+#         Human readable string describing the exception.
     
-    """
+#     """
 
-    def __init__(self, msg: str):
-        """Set the error message.
-        Parameters
-        ----------
-        msg : `str`
-            Human readable string describing the exception.
+#     def __init__(self, msg: str):
+#         """Set the error message.
+#         Parameters
+#         ----------
+#         msg : `str`
+#             Human readable string describing the exception.
         
-        """
-        self.msg = msg
+#         """
+#         self.msg = msg
     
-    def __str__(self):
-        """Return the error message."""
-        return self.msg
+#     def __str__(self):
+#         """Return the error message."""
+#         return self.msg
 
-class InvalidSettingError(ErrorNCM6212C):
-    """Raised when an invalid parameter is set."""
+# class InvalidSettingError(ErrorNCM6212C):
+#     """Raised when an invalid parameter is set."""
 
 
 if __name__ == "__main__":
