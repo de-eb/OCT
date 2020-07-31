966153662a7af1069054dc1fd7b3863a323b5b28
kawashima <kawashima.takuya.bl@tut.jp>
2020-07-31 16:35:10 +0900

    [update] test measurements

--- measure.py
+++ measure.py
@@ -1,23 +1,37 @@
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 from modules.c10439 import C10439_11
 from modules.fine01r import FINE01R
 
 if __name__ == "__main__":
 
-    photo = C10439_11(ai_channels="Dev1/ai2")  # initialize photo detector
-    stage = FINE01R('COM4')  # initialize piezo stage
+    #  Device settings
+    photo = C10439_11(ai_channels="Dev1/ai2")  # photo detector
+    stage = FINE01R('COM4')  # piezo stage
 
-    data = np.zeros((2,100))
+    # Data container
+    position = np.arange(start=0, stop=2000, step=10)  # stage position
+    voltage = np.zeros_like(position, dtype=float)  # photo detector output
 
-    for i in range(data.shape[1]):
-        stage.absolute_move(i)
+    # Measuring
+    stage.absolute_move(0)
+    time.sleep(5)
+    for i in range(len(position)):
+        stage.absolute_move(position[i])
         print(stage.read_status())
-        time.sleep(1)
-        data[0,i] = i  # stage position
-        tmp = photo.read_voltage(samples=100)[1]
-        data[1,i] = np.mean(tmp)
-        print(data[1,i])
+        time.sleep(2)
+        voltage[i] = np.mean(photo.read_voltage(samples=1000)[1])
+        print(voltage[i])
     
-    photo.stop_measuring()
-    np.savetxt('data.csv', data.T, delimiter=',')
+    # Save data
+    data = np.vstack((position, voltage)).T
+    np.savetxt('data/data.csv', data, delimiter=',')
+
+    # Show Graph
+    fig, ax = plt.subplots(1, 1)
+    ax.set_title("Measurement results")
+    ax.set_xlabel("stage position [nm]")
+    ax.set_ylabel("voltage [V]")
+    ax.plot(position, voltage)
+    plt.show()
