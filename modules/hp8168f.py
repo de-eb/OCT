d86573b301bc64960a949bf9d4afd0e253f8cde2
takuya <tkykwsm6ch@gmail.com>
2020-08-04 23:40:31 +0900

    [update] The commands are simplified.

--- modules/hp8168f.py
+++ modules/hp8168f.py
@@ -1,125 +1,69 @@
-import time
-from enum import Enum
 import pyvisa
 
 
 class HP8168F:
 
-    class Switch(Enum):
-        ON = 'ON'
-        OFF = 'OFF'
-
-    class SetTo(Enum):
-        MINIMUM = 'MIN'
-        DEFAULT = 'DEF'
-        MAXIMUM = 'MAX'
-    
-    class PowerUnits(Enum):
-        DBM = 'DBM'
-        WATT = 'W'
-
-    def __init__(self, gpib_id: str, delimiter='\n'):
-
-        self.__rm = pyvisa.ResourceManager()
-        # print(self.__rm.list_resources())
-        self.__dev = self.__rm.open_resource(gpib_id)
-        self.__dev.read_termination = delimiter
-        self.__dev.write_termination = delimiter
-    
-    def read_hardware_info(self):
-        """ The identication query commands the instrument
-            to identify  itself over the interface.
-        """
-        hardware_info = self.__dev.query('*IDN?')
-        return hardware_info
-    
-    def lock_device(self, lock: bool, password='8168') -> bool:
-        """ Switches the device-lock OFF and ON.
-        """
-        self.__dev.write(
-            ':LOCK {},{}'.format('ON' if lock else 'OFF', password))
-        ret = self.__dev.query(':LOCK?')
-        if ret == '+1':
-            return True
-        elif ret == '+0':
-            return False
-    
-    def output(self, output: bool):
-        """ Switches the laser current OFF and ON.
-        """
-        self.__dev.write(':OUTP {}'.format('ON' if output else 'OFF'))
-    
-    def set_output_mode(self, attenuation: bool):
-        """ Selects Power or Attenuation Mode. 
-            In Power Mode, you specify the output power.
-            In Attenuation Mode, you must specify both the laser output power
-            and the attenuation level.
-        """
-        self.__dev.write(
-            ':POW:ATT:AUTO {}'.format('ON' if attenuation else 'OFF'))
-        mode = self.__dev.query(':POW:ATT:AUTO?')
-        return mode
-    
-    def set_power_units(self, unit: Enum):
-        """ Sets the power units.
-        """
-        self.__dev.write(':POW:UNIT {}'.format(unit))
-    
-    def set_power(self, val):
-        """ Sets the power.
-        """
-        self.__dev.write(':POW {}'.format(val))
-    
-    def adjust(
-        self, power: float, attenuation: float, wavelength: float):
-        """ Sets the intensity, wavelength and attenuation of the laser.
+    def __init__(self, gpib_id: str, pin='8168'):
+        """ Initiates and unlocks communication with the device.
 
         Parameters
         ----------
-        power : `float` or `Enum`
-            laser power(dBm). Set
-        
-        level : `float` or `Enum`
-            Level of attenuation(dB). Set from 1.5dB to -47dBm
-        
-        wavelength : `float` or `Enum`
-            wavelength(nm) of laser. Set from 1450nm to 1590nm
-        """
-        if isinstance(power, HP8168F.SetTo):
-            self.__dev.write(':POW {}'.format(power))
-        else:
-            self.__dev.write(':POW: {}DBM'.format(power))
+        gpib_id : `str`, required
+            GPIB address of the device.
 
-        if isinstance(attenuation, HP8168F.SetTo):
-            self.__dev.write(':POW:ATT {}'.format(attenuation))
-        else:
-            self.__dev.write(':POW:ATT {}DB'.format(attenuation))
+        pin : `str`, optional
+            Four-character PIN to unlock the device. Default is 8168.
+        """
+        self.__rm = pyvisa.ResourceManager()
+        # print(self.__rm.list_resources())  # Get a list of GPIB addresses.
+        self.__dev = self.__rm.open_resource(gpib_id)
+        self.__dev.read_termination = '\n'  # Set the delimiter
+        self.__dev.write_termination = '\n'
+        print(self.__dev.query('*IDN?'))  # Identify the device.
+        self.__dev.write(':LOCK OFF,{}'.format(pin))
+        self.__dev.write(':POW:UNIT W')  # Set the unit of power to Watt.
+    
+    def output(self, wavelength: float, power: float):
+        """ Output laser.
         
-        if isinstance(wavelength, HP8168F.SetTo):
-            self.__dev.write(':WAVE {}'.format(wavelength))
-        else:
-            self.__dev.write(':WAVE {}NM'.format(wavelength))
+        Parameters
+        ----------
+        wavelength : `float`, required
+            Laser wavelength [nm]. It can be set between 1450 ~ 1590nm.
+
+        power : `float`, required
+            Laser power intensity [W].
+        """
+        self.__dev.write(':WAVE {}NM'.format(wavelength))
+        self.__dev.write(':POW {}W'.format(power))
+        self.__dev.write(':OUTP ON')
+
+    def stop(self):
+        """ Stop the laser output.
+        """
+        self.__dev.write(':OUTP OFF')
     
     def read_status(self):
+        """ Reads the status of the device.
+
+        Returns
+        -------
+        `dict` Name and data pairs.
+
+            {
+                'output'    : bool State of laser output.
+                'wavelength': float Laser wavelength [nm].
+                'power'     : Laser power intensity [W].
+            }
         """
-        """
-        stat = {}
-        stat['output'] = self.__dev.query(':OUTP?')
-        # stat['blind'] = self.__dev.query(':POW:ATT:DARK?')
-        # stat['mode'] = self.__dev.query(':POW:ATT:AUTO?')
-        stat['unit'] = self.__dev.query(':POW:UNIT?')
-        stat['power'] = self.__dev.query(':POW?')
-        # stat['attenuation'] = self.__dev.query(':POW:ATT?')
-        stat['wavelength'] = self.__dev.query(':WAVE?')
-        return stat
+        status = {}
+        status['output'] = self.__dev.query(':OUTP?')
+        status['wavelength'] = self.__dev.query(':WAVE?')
+        status['power'] = self.__dev.query(':POW?')
+        return status
 
 
 if __name__ == "__main__":
 
-    laser = HP8168F('GPIB0::24::INSTR')
-    print(laser.read_hardware_info())
-    if not laser.lock_device(lock=False, password=0000):
-        # laser.set_output_mode(attenuation=False)
-        laser.set_power('MAX')
-        laser.output(True)
-        print(laser.read_status())
+    laser = HP8168F(gpib_id='GPIB0::24::INSTR', pin=0000)
+    print(laser.read_status())
