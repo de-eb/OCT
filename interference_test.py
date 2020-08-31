15ae7fcbbc1f06f92767cf160616c144e19100c8
kawashima <kawashima.takuya.bl@tut.jp>
2020-08-31 15:42:40 +0900

    [update] add comment

--- interference_test.py
+++ interference_test.py
@@ -18,16 +18,18 @@ if __name__ == "__main__":
     position = np.arange(start=0, stop=2000, step=10)  # stage position
     voltage = np.zeros_like(position, dtype=float)  # photo detector output
 
-    # Measuring
+    # Initializing
     laser.output(wavelength=1500, power=3000)
     stage1.absolute_move(0)
     stage2.absolute_move(axis='A', position=0)
     stage2.absolute_move(axis='B', position=0)
     time.sleep(5)
+
+    # Measuring
     for i in range(len(position)):
         # Be sure to comment out one or the other.
-        stage1.absolute_move(position[i])
-        # stage2.absolute_move(axis='A', position=position[i])
+        # stage1.absolute_move(position[i])
+        stage2.absolute_move(axis='A', position=position[i])
 
         voltage[i] = np.mean(photo.read_voltage(samples=1000)[1])
         print('{} nm, {:.3f} V'.format(position[i],voltage[i]))
