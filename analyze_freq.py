870d455314a9b4bf24c97ebfa7fca686e8dd7ade
kawashima <kawashima.takuya.bl@tut.jp>
2020-09-26 13:19:33 +0900

    [update]add refractiveindex and adjust x-axis

--- analyze_freq.py
+++ analyze_freq.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from scipy.fftpack import ifft
     
 # Data loading
-data = np.loadtxt('data/200908_0.csv', delimiter=',')
+data = np.loadtxt('data/200925_2.csv', delimiter=',')
 freq = data[:,0]*1e9 # frequency[GHz]
 itf = data[:,1]  # interference spectra
-ref = np.loadtxt('data/200908_0_ref.csv', delimiter=',')[:,1]  # reference spectra
+ref = np.loadtxt('data/200925_2_ref.csv', delimiter=',')[:,1]  # reference spectra
+n=1.5 #refractive index
+
 
 # Background Subtraction
 itf_BGremoved = itf - ref #interference spectra(background removed)
@@ -19,7 +20,7 @@ data=np.abs(np.fft.ifft(itf_BGremoved))
 time=np.arange(0,len(freq))
 timeband=1./(np.amax(freq)-np.amin(freq))#時間軸刻み幅
 time=timeband*time #ここで[psec]
-depth=((time*3e8)/2)*1e3
+depth=((time*3e8)/2*n)*1e3
 depth=depth-np.amin(depth)
 depth=depth-depth[int(len(freq)/2)]
 
@@ -28,4 +29,5 @@ plt.xlabel("depth[mm]")
 plt.ylabel("light intensity")
 plt.plot(depth,data)
 plt.xlim(0,np.amax(depth))
+plt.xticks(np.arange(0,10+1,1))
 plt.show()
