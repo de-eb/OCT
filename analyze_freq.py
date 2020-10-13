e0971d111892c813804cbb3462ed44ef7fc0b8d3
hamasax42193 <hamasax42193@gmail.com>
2020-10-13 15:01:41 +0900

    [update]test commit

--- analyze_freq.py
+++ analyze_freq.py
@@ -1,24 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import ifft
-    
+
 # Data loading
-data = np.loadtxt('data/200925_2.csv', delimiter=',')
+data = np.loadtxt('data/201009_6.csv', delimiter=',')
 freq = data[:,0]*1e9 # frequency[GHz]
 itf = data[:,1]  # interference spectra
-ref = np.loadtxt('data/200925_2_ref.csv', delimiter=',')[:,1]  # reference spectra
+ref = np.loadtxt('data/201009_ref2.csv', delimiter=',')[:,1]  # reference spectra
 n=1.5 #refractive index
-data_miss=np.loadtxt('data/200917_correct.csv',delimiter=',',encoding='utf-8_sig')#レーザー強度の波長依存性のデータ
-miss=data_miss[:,1]#レーザー強度の波長依存性のデータ,光強度だけ
-correction=miss/np.average(miss) #測定データからレーザー強度の波長依存性を取り除くための行列
-
-# Background Subtraction
-itf_BGremoved = itf - ref #interference spectra(background removed)
-itf_BGremoved_corrected=itf_BGremoved*correction
+itf_BGremoved=itf-ref
 
 # inverse fft
-data=np.abs(np.fft.ifft(itf_BGremoved_corrected))
-data2=np.abs(np.fft.ifft(itf_BGremoved))
+data=np.abs(np.fft.ifft(itf_BGremoved))
 
 # x-axis calculation
 time=np.arange(0,len(freq))
@@ -31,8 +24,7 @@ depth=depth-depth[int(len(freq)/2)]
 
 plt.xlabel("depth[mm]")
 plt.ylabel("light intensity")
-plt.plot(depth,data,label='corrected')
-plt.plot(depth,data2,label='non-corrected')
+plt.plot(depth,data)
 plt.xlim(0,np.amax(depth))
 plt.legend()
 plt.show()
