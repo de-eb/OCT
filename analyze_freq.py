c397aacf2089032b325c235b15b055e1af9dfeb2
hamasax42193 <hamasax42193@gmail.com>
2020-10-14 15:15:22 +0900

    [update]simplify time calculation statement

--- analyze_freq.py
+++ analyze_freq.py
@@ -14,17 +14,14 @@ itf_BGremoved=itf-ref
 data=np.abs(np.fft.ifft(itf_BGremoved))
 
 # x-axis calculation
-time=np.arange(0,len(freq))
-timeband=1./(np.amax(freq)-np.amin(freq))#時間軸刻み幅
-time=timeband*time #ここで[psec]
+time=1./(np.amax(freq)-np.amin(freq))*np.arange(0,len(freq))
 depth=((time*3e8)/2*n)*1e3
 depth=depth-np.amin(depth)
 depth=depth-depth[int(len(freq)/2)]
-
 
 plt.xlabel("depth[mm]")
 plt.ylabel("light intensity")
 plt.plot(depth,data)
 plt.xlim(0,np.amax(depth))
-plt.legend()
+plt.ylim(0,0.04)
 plt.show()
