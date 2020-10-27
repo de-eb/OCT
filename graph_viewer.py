d7ec5b4b613c7c20fcc602bbd0578d46dd814d6b
takuya <tkykwsm6ch@gmail.com>
2020-10-27 09:40:04 +0900

    [update]

--- graph_viewer.py
+++ graph_viewer.py
@@ -1,12 +1,13 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
     # Data loading
-    data = np.loadtxt('data/201003_7.csv', delimiter=',')
-    position = data[:,0]
-    voltage = data[:,1]
+    data = pd.read_csv('data/201023_2.csv', header=2, index_col=0)
+    position = data.values[:,0]
+    voltage = data.values[:,1]
 
     # Calculate theoretical curve
     ref = (np.cos(2*np.pi*(position-position[np.argmax(voltage)])/1540))**2
@@ -14,7 +15,7 @@ if __name__ == "__main__":
 
     # Show Graph
     fig, ax = plt.subplots(1, 1)
-    ax.set_title("Interference fringes of Coverslip(0.148mm) at 1540nm")
+    ax.set_title("Interference fringes at 1540nm with EL-NIKKOR 135mm 1:5.6")
     ax.set_xlabel("stage position [nm]")
     ax.set_ylabel("voltage [V]")
     ax.scatter(position, voltage, s=20, label='measured')
--- signal_processing.py
+++ signal_processing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy import special
 import matplotlib.pyplot as plt
 
@@ -6,55 +7,75 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.family'] ='sans-serif'
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams["xtick.minor.visible"] = True
+plt.rcParams["ytick.minor.visible"] = True
 plt.rcParams['xtick.major.width'] = 1.0
 plt.rcParams['ytick.major.width'] = 1.0
+plt.rcParams["xtick.minor.width"] = 0.5
+plt.rcParams["ytick.minor.width"] = 0.5
 plt.rcParams['font.size'] = 14
 plt.rcParams['axes.linewidth'] = 1.0
 
 # Constants
 c = 2.99792458e8  # Speed of light in a vacuum [m/sec].
-n = 1.5  # Refractive index of the sample. cellulose = 1.46
+n = 1.4  # Refractive index of the sample. cellulose = 1.46
+alpha = 1.5  # design factor of Kaiser window
 
 if __name__ == "__main__":
 
     # Data loading
-    ref = np.loadtxt('data/201009_ref2.csv', delimiter=',')
-    freq = ref[:,0]*1e9 # frequency
-    bg = ref[:,1]  # background spectra
-    raw = np.loadtxt('data/201009_6.csv', delimiter=',')[:,1]  # sample spectra
+    ref = pd.read_csv('data/201015_ref.csv', header=2, index_col=0)
+    data0 = pd.read_csv('data/201016_1.csv', header=2, index_col=0)
+    data1 = pd.read_csv('data/201023_0.csv', header=2, index_col=0)
+    freq = ref.values[:,0]*1e9 # frequency
+    bg = ref.values[:,1].reshape((len(freq),1))  # background spectra
+    raw0 = data0.values[:,1:].reshape((len(freq),-1))  # sample spectra
+    raw1 = data1.values[:,1:].reshape((len(freq),-1))
 
-    # Background subtruction
-    sub = raw/bg
+    # Normalize
+    sub0 = raw0/bg
+    sub1 = raw1/bg
 
     # Re-sampling
     x = np.linspace(0, len(freq), len(freq))
     # window = 0.42 - 0.5*np.cos(2*np.pi*x) + 0.08*np.cos(4*np.pi*x)  # Blackman window
-    alpha = 1.5
     window = special.iv(0, np.pi*alpha*np.sqrt(1-(2*x/len(x)-1)**2)) / special.iv(0, np.pi*alpha)  # Kaiser window
-    wnd = sub*window
+    window = window.reshape((len(freq),1))
+    wnd0 = sub0*window
+    wnd1 = sub1*window
+    wnd2 = bg*window
 
     # x-axis calculation
     centre = int(len(freq)/2)
-    time = 1. / (np.amax(freq)-np.amin(freq)) * np.arange(0,len(freq))
-    # time = 1. / freq
-    depth = time*c / (2*n)
-    depth = depth - np.amin(depth)
-    depth = depth - depth[centre]
+    time = np.arange(len(freq))/(len(freq)*(freq[1]-freq[0]))
+    depth = time*c/(2*n) * 1e3
+    depth = depth[centre:] - depth[centre]
+
+    # fs = 2*np.amax(freq)  # sampling freq
+    # d = len(freq)/fs  # time window length
+    # depth = np.linspace(0, d*c/(2*n), len(freq))*1e3  # [mm]
+    # depth = depth[centre:] - depth[centre]
 
     # FFT
-    fft = np.abs(np.fft.ifft(wnd))
+    fft0 = np.abs(np.fft.ifft(wnd0, axis=0))[centre:,:]
+    fft1 = np.abs(np.fft.ifft(wnd1, axis=0))[centre:,:]
+    fft2 = np.abs(np.fft.ifft(wnd2, axis=0))[centre:,:]
 
     # Show Graph
     fig = plt.figure(figsize=(10, 10), dpi=80, tight_layout=True)
-    ax1 = fig.add_subplot(311, title='Interferogram', xlabel='Frequency [THz]', ylabel='Voltage [V]')
-    ax1.plot(freq*1e-12, bg, label='Reference', color=(1,0,0,0.5))
-    ax1.plot(freq*1e-12, raw, label='Sample (coverslip)', color=(0,0,1,0.5))
-    ax1.legend()
-    ax2 = fig.add_subplot(312, title='Re-sampling', xlabel='Frequency [THz]', ylabel='Intensity [-]')
-    ax2.plot(freq*1e-12, sub, label='Normalized', color=(1,0,0,0.5))
-    ax2.plot(freq*1e-12, wnd, label='Kaiser α=1.5', color=(0,0,1,0.5))
-    ax2.legend()
-    ax3 = fig.add_subplot(313, title='A-scan', xlabel='Depth [mm]', ylabel='Intensity [-]')
-    ax3.plot(depth[centre:]*1e3, fft[centre:], color=(0,0,1,0.5))
+    ax0 = fig.add_subplot(211, title='A-scan change with sample displacement (coverslip, in the optical axis)', xlabel='Depth [mm]', ylabel='Intensity [-]')
+    ax0.plot(depth, fft0[:,0]+0.09, label='0μm', color=(1,0,0,0.5))
+    ax0.plot(depth, fft0[:,1]+0.06, label='1μm', color=(0,0,1,0.5))
+    ax0.plot(depth, fft0[:,2]+0.03, label='2μm', color=(0,1,0,0.5))
+    ax0.plot(depth, fft2, label='BG', color=(0,0,0,0.5))
+    ax0.grid(which='both', axis='x', color='gray', alpha=0.3, linestyle='-', linewidth=0.5)
+    ax0.legend()
+    
+    ax3 = fig.add_subplot(212, title='A-scan change with sample displacement (cellophan tape, in the optical axis)', xlabel='Depth [mm]', ylabel='Intensity [-]')
+    ax3.plot(depth, fft1[:,0]+0.09, label='0μm', color=(1,0,0,0.5))
+    ax3.plot(depth, fft1[:,1]+0.06, label='1μm', color=(0,0,1,0.5))
+    ax3.plot(depth, fft1[:,2]+0.03, label='2μm', color=(0,1,0,0.5))
+    ax3.plot(depth, fft2, label='BG', color=(0,0,0,0.5))
+    ax3.grid(which='both', axis='x', color='gray', alpha=0.3, linestyle='-', linewidth=0.5)
     ax3.legend()
     plt.show()
