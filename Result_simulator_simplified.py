c63a33fcc370bda9894fdab279c97d64bb292b6f
hamasax42193 <hamasax42193@gmail.com>
2021-01-12 10:40:59 +0900

    [update]add 5th glass

--- Result_simulator_simplified.py
+++ Result_simulator_simplified.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 c0=299792458  #speed of light in vacuum[m/sec]
 n1=1.0        #refractive index of air
 n2=1.5        #refractive index of glass 
-tg=[0.1e-3,0.5e-3,1e-3,2e-3]       #thickness of glass[m]
-condition=['0.1[mm]','0.5[mm]','1.0[mm]','2.0[mm]']
+tg1=1e-3      #thickness of 1st glass[m]
+tg2=0.5e-3      #thickness of 2nd glass[m]
+tg3=0.5e-3      #thickness of 3rd glass[m]
+tg4=0.3e-3   #thickness of 4th glass[m]
+tg5=0.2e-3   #thickness of 5th glass[m]
 fmin=189.7468 #minimum sweep frequency[THz]
 fmax=203.2548 #maximum sweep frequency[THz]
 fstep=2e-3  #sweep frequency step[THz]
-xmax=2.5e-3    #x-axis length[m]
+xmax=3e-3    #x-axis length[m]
 phase_diff=0 #phase difference between reference and sample light
 
 c=c0/n2               #speed of light in glass[m/sec]
@@ -27,31 +30,39 @@ ref=np.sin(one_cycle) #reference light
 x=np.linspace(0,1,len(freq))
 wf=0.42-0.5*np.cos(2*np.pi*x)+0.08*np.cos(4*np.pi*x)
 
-for i in range(len(tg)):
+for i in range(1):
     for j in range(len(freq)):
         wl_g=c/freq[j]*1e-12 #wavelength of incident wave[m](glass)
 
         #light from surface
-        #light_sur=np.sin(one_cycle+phase_diff)
-        light_sur=0
+        light_sur=np.sin(one_cycle+phase_diff)
+        #light_sur=0
 
-        #light throught the glass
-        lp=((tg[i]%wl_g)/wl_g)*2*np.pi
-        #light_thr=np.sin(one_cycle+phase_diff+lp)
-        light_thr=0
+        #light throught the 1st glass
+        lp1=((tg1%wl_g)/wl_g)*2*np.pi
+        light1=np.sin(one_cycle+phase_diff+lp1)
+        #light_thr=0
 
-        check=(ref+light_sur+light_thr)**2
+        #light throught the 2nd glass
+        lp2=((tg1+tg2)%wl_g)/wl_g*2*np.pi
+        light2=np.sin(one_cycle+phase_diff+lp2)
+
+        #light throught the 3rd glass
+        lp3=((tg1+tg2+tg3)%wl_g)/wl_g*2*np.pi
+        light3=np.sin(one_cycle+phase_diff+lp3)
+
+        #light throught the 4th glass
+        lp4=((tg1+tg2+tg3+tg4)%wl_g)/wl_g*2*np.pi
+        light4=np.sin(one_cycle+phase_diff+lp4)
+
+        #light throught the 5th glass
+        lp5=((tg1+tg2+tg3+tg4+tg5)%wl_g)/wl_g*2*np.pi
+        light5=np.sin(one_cycle+phase_diff+lp5)
+        
+
+        check=(ref+light_sur+light1+light2+light3+light4+light5)**2
         itf[j]=np.amax(check)-1
         itf_wf=itf*wf
-
-        #if j%100==0:
-        #    print(freq[j])
-        #    plt.plot(one_cycle,ref,label='reference light')
-        #    plt.plot(one_cycle,light_sur,label='light from surface')
-        #    plt.plot(one_cycle,light_thr,label='light throught the glass')
-        #    plt.legend()
-        #    plt.xlabel('phase')
-        #    plt.show()
 
     #inverse ft
     for j in range(len(freq)):
@@ -60,8 +71,8 @@ for i in range(len(tg)):
         else:
             result+=itf_wf[j]*np.sin(2*np.pi*time*freq[j]*1e12)
     result/=len(freq)
-    plt.plot(depth,abs(result),label=condition[i])
+    plt.plot(depth,abs(result))
 plt.legend()
 plt.xlabel('depth[mm]')
+plt.xticks(np.arange(0,xmax*1e3,0.5))
 plt.show()
-
