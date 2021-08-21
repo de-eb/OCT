367bc193fdf6b20f286d2f667167f4af59892c77
hamasax42193 <hamasax42193@gmail.com>
2021-08-21 17:01:37 +0900

    [update]adjust unit and remove ifh

--- analyse_master's_thesys.py
+++ analyse_master's_thesys.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import inverseFourier_hamasaki as ifh 
 
 data=np.loadtxt("data/210729.csv",delimiter=",",encoding='utf-8_sig')
 data_nosample=np.loadtxt("data/210729_ref.csv",delimiter=",",encoding='utf-8_sig')
@@ -9,17 +8,15 @@ no_sample=data_nosample[:,1]
 itf = data[:,1]  # interference spectra
 itf-=no_sample
 
-
-
 x=np.linspace(0,1,len(freq))
 wf=0.42-0.5*np.cos(2*np.pi*x)+0.08*np.cos(4*np.pi*x)
 itf_wf=itf*wf
 c0=299792458  #speed of light in vacuum[m/sec]
 
 #x-axis calculation
-xmax=1e-3
+xmax=1 #x-axis maximum value[mm]
 n1=1.4
-depth_axis=np.linspace(0,xmax*1e3,int(1e2))
+depth_axis=np.linspace(0,xmax,int(1e2))
 time=2*(n1*depth_axis*1e-3)/c0
 
 for j in range(len(freq)):
