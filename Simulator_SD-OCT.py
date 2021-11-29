254af6d27ff29926a4854328d5bd53cd2102e45a
MizobeK <mizobe.kosuke.wa@tut.jp>
2021-11-29 15:14:49 +0900

    [update]

--- Simulator_SD-OCT.py
+++ Simulator_SD-OCT.py
@@ -66,14 +66,15 @@ def inverse_ft(freq, itf, xmax, n):
     return depth_axis, abs(result)
 # Constants
 c0 = 299792458                          # speed of light in vacuum[m/sec]
-n1 = 1.00                               # refractive index of air
-n2 = 1.29                               # refractive index of cellophane 
+n0 = 1.00                               # refractive index of air
+n1 = 1.29                               # refractive index of csample1
+n2 = 1.29                               # refractive index of csample2
 ta1 = 150e-3                            # thickness of air
-ta2 = 10e-6                             # thickness between samples
-tc = 30e-6                              # thickness of sample
+ta2 = 20e-6                             # thickness between samples
+tc1 = 30e-6                             # thickness of sample1
+tc2 = 30e-6                             # thickness of sample2
 
-# Memo
-# GR...st=200 ed=667 (350~700) WH...st=200 ed=900 (350~860) FL...st=404 ed=613
+# Memo : GR...st=200 ed=667 (350~700) WH...st=200 ed=900 (350~860) FL...st=404 ed=613
 st = 200
 ed = 667
 
@@ -86,9 +87,14 @@ sp = data.loc[st:ed,'sp']               # sample spectras
 sp = np.sqrt(sp)                        # amplitude
 
 # Calculation base on constants
-R = ((n2-n1)/(n1+n2))**2                # reflectance
-T = 1-R                                 # transmittance
-wl_c = wl/n2                            # wavelength in cellophane
+R0 = ((n1-n0)/(n0+n1))**2                # reflectance air and sample1
+T0 = 1-R0                                # transmittance
+R1 = ((n2-n1)/(n1+n2))**2                # reflectance sample1 and sample2
+T1 = 1-R1                                # transmittance
+R2 = ((n0-n2)/(n2+n0))**2                # reflectance air and sample2
+T2 = 1-R2                                # transmittance
+wl_1 = wl/n1                             # wavelength in sample1
+wl_2 = wl/n2                             # wavelength in sample2
 phase_diff = (ta1%wl)/wl*2*np.pi+np.pi
 one_cycle = np.arange(0, 1, 1e-3)*2*np.pi
 itf = np.empty_like(wl)
@@ -96,22 +102,22 @@ ref = np.empty_like(wl)
 
 # Calculation the interference
 for i in range(len(wl)):
-    light_ref = 3*R*sp.values[i]*np.sin(one_cycle+phase_diff.values[i]+0*one_cycle)
+    light_ref = 30*R0*sp.values[i]*np.sin(one_cycle+phase_diff.values[i]+0*one_cycle)
 
     # Light from surface of 1st sample
-    light_s1 = sp.values[i]*R*np.sin(one_cycle+phase_diff.values[i]+np.pi)
+    light_s1 = sp.values[i]*R0*np.sin(one_cycle+phase_diff.values[i]+np.pi)
 
     # Light throught the 1st sample
-    lp1 = (((2*tc)%wl_c.values[i])/wl_c.values[i])*2*np.pi
-    light1 = sp.values[i]*T**2*R*np.sin(one_cycle+phase_diff.values[i]+lp1)
+    lp1 = (((2*tc1)%wl_1.values[i])/wl_1.values[i])*2*np.pi
+    light1 = sp.values[i]*T0**2*R0*np.sin(one_cycle+phase_diff.values[i]+lp1)
 
     # Light from surface of 2nd sample
     lp2 = (((2*ta2)%wl.values[i])/wl.values[i])*2*np.pi
-    light_s2 = sp.values[i]*T**4*R*np.sin(one_cycle+phase_diff.values[i]+np.pi+lp1+lp2)
+    light_s2 = sp.values[i]*(T0**2)*(T0**2)*R2*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2+np.pi)
 
     # Light throught the 2nd sample
-    lp3 = (((2*tc)%wl_c.values[i])/wl_c.values[i])*2*np.pi
-    light2 = sp.values[i]*T**6*R*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2+lp3)
+    lp3 = (((2*tc2)%wl_2.values[i])/wl_2.values[i])*2*np.pi
+    light2 = sp.values[i]*(T0**4)*(T2**2)*R2*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2+lp3)
     
     # check = (light_ref+light_s1+light1)**2                     # sample=1
     
@@ -128,7 +134,7 @@ itf_new = itf - ref
 freq_fixed,itf_fixed = Resampling(wl,itf_new)
 
 # Generate ascan
-depth,result = inverse_ft(freq_fixed*1e-9, itf_fixed, 0.1, n2)
+depth,result = inverse_ft(freq_fixed*1e-9, itf_fixed, 0.1, n1)
 
 # Show graphs
 plt.plot(wl*1e9, sp**2)
