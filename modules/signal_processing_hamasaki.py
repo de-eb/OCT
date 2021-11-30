93c18ad25112cd87b4860f43a683fc358a65cfa7
hamasax42193 <hamasax42193@gmail.com>
2021-11-30 16:20:11 +0900

    多分完成した

--- modules/signal_processing_hamasaki.py
+++ modules/signal_processing_hamasaki.py
@@ -1,108 +1,77 @@
 import numpy as np
+from numpy.lib.type_check import asscalar
 from pycubicspline import *
 
 class SignalProcessor_hamasaki():
     """
-    A class that packages variou types of signal processing for OCT.
+    A class that packages various types of signal processing for OCT.
     """
     c = 2.99792458e8  # Speed of light in vacuum [m/sec].
 
     def __init__(self,wl,n,xmax,sl):
-        """
-        Initialization and preprocessing of parameters.
-
-        Parameters
-        ----------
-        wl : `1d-ndarray`, required
-            Wavelength axis[nm] The given spectra must be sampled evenly in wavelength space.
-        n : `float`, required
-            Refractive index of the sample .
-        xmax : 'float', required
-            maximum value of depth axis[mm]
-        sl :  `float`, required
-            Signal length.
-            The calculation result always be periodic function. 
-            This parameter controls the length of the cycle.
-            The higher this parameter, the longer the period, but also the longer the time required for the calculation.
-
-        """
         # Axis conversion for resampling
         self.__wl=wl
         self.__depth=np.linspace(0, xmax, int(1e5))
         self.__time=2*(n*self.__depth*1e-3)/SignalProcessor_hamasaki.c
-        freqmin=(SignalProcessor_hamasaki.c/(1e9*np.amax(self.__wl)))*1e6-1
-        freqmax=(SignalProcessor_hamasaki.c/(1e9*np.amin(self.__wl)))*1e6+1
-        self.__freq=np.linspace(freqmin,freqmax,int(len(self.__wl)*sl))
-        
+        self.__freq=(SignalProcessor_hamasaki.c/(self.__wl*1e9))*1e6
+        self.__freq_fixed=np.linspace(np.amin(self.__freq)-1,np.amax(self.__freq)+1,int(len(self.__wl)*sl))
         #initialize data container
         self.__ref=None
 
     @property
     def depth(self):
-        """ Horizontal axis after FFT (depth [m])
-        """
         return self.__depth
 
     def Resampling(self,sp):
-        """Resampling function for OCT.
-    
-        Parameters
-        ----------
-        sp : `1d-ndarray`
-            measured interference data[arb. unit]
-        
-        Returns
-        -------
-        `1d-ndarray`
-        Spectra resampled evenly in the frequency space.
-        
-        Requirement
-        -------
-        pycubicspline.py (from pycubicspline import *)
-        """
+        spline=Spline(np.flipud(self.__freq),np.flipud(sp))
+        itf_fixed=[spline.calc(i) for i in self.__freq_fixed]
+        for i in range(len(itf_fixed)):
+            if itf_fixed[i]==None:
+                itf_fixed[i]=0
+        return itf_fixed
 
     def set_reference(self,ref):
-        """ Specify the reference spectra. This spectra will be used in later calculations.
-
-        Parameters
-        ----------
-        spectra : `1d-ndarray`, required
-            Spectra of reference light only, sampled evenly in wavelength space.
-        """
         self.__ref=self.Resampling(ref)
 
-
     def BGsubtraction(self,sp):
-        """Subtract reference light from interference light.
-    
-        Parameters
-        ----------
-        sp : `1d-ndarray`, required
-            Spectra. Normally, specify the interference spectra after resampling.
-
-        
-        Return
-        -------
-        `1d-ndarray`
-            interference light removed background[arb. unit]
-        """
-        return sp-self.__ref*(np.amax(sp)/np.amax(self.__ref))
+        return sp-np.multiply(self.__ref,(np.amax(sp)/np.amax(self.__ref)))
 
     def inverse_ft(self,sp):
-        """Apply inverse ft to the spectra and convert it to time domain data
+        for  i in range(len(self.__freq_fixed)):
+            if i==0:
+                result=sp[i]*np.sin(2*np.pi*self.__time*self.__freq_fixed[i]*1e12)
+            else:
+                result+=sp[i]*np.sin(2*np.pi*self.__time*self.__freq_fixed[i]*1e12)
+        result/=np.amax(result)
+        return abs(result)
 
-        Parameters
-        ----------
-        sp : `1d-ndarray`, required
-            spectra(After applying resampling)
+    def generate_ascan(self,interference,reference):
+        if self.__ref==None:
+            self.set_reference(reference)
+        itf=self.Resampling(interference)
+        rmv=self.BGsubtraction(itf)
+        ascan=self.inverse_ft(rmv)
+        return self.__depth,ascan
 
-        Returns
-        ----------
-        `1d-array`
-            Data after IFFT
-        
-        """
+if __name__=="__main__":
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
+    st = 333
+    ed = 533
+    name=['wl','bg','sp']
+    data=pd.read_csv('data/211103_0.csv', header=3, index_col=0,names=name)
+    wl=data.loc[st:ed,'wl'] # Wavelength
+    bg=data.loc[st:ed,'bg'] # Background spectra
+    sp=data.loc[st:ed,'sp'] # Sample spectra
+
+    SigPro=SignalProcessor_hamasaki(wl,1.4,0.2,3)
+    depth,result=SigPro.generate_ascan(sp,bg)
+    plt.plot(depth,result)
+    plt.show()
+
+
+'''
 def BGsubtraction(sp,bg):
     """Subtract reference light from interference light.
     
@@ -154,6 +123,15 @@ def Resampling(wl,itf):
     return freq_fixed,itf_fixed
 
 def inverse_ft(freq, itf, xmax, n):
+    depth = np.linspace(0, xmax, int(1e5))
+    time = 2*(n*depth*1e-3)/299792458
+    for i in range(int(len(freq))):
+        if i==0:
+            result = itf[i]*np.sin(2*np.pi*time*freq[i]*1e12)
+        else:
+            result += itf[i]*np.sin(2*np.pi*time*freq[i]*1e12)
+    result /= np.amax(result)
+    return depth, abs(result)
     """Inverse Fourier transform function for OCT.
     
         Parameters
@@ -174,15 +152,7 @@ def inverse_ft(freq, itf, xmax, n):
         result : `ndarray`
             transformed data[arb. unit]
     """
-    depth = np.linspace(0, xmax, int(1e5))
-    time = 2*(n*depth*1e-3)/299792458
-    for i in range(int(len(freq))):
-        if i==0:
-            result = itf[i]*np.sin(2*np.pi*time*freq[i]*1e12)
-        else:
-            result += itf[i]*np.sin(2*np.pi*time*freq[i]*1e12)
-    result /= np.amax(result)
-    return depth, abs(result)
+
 
 if __name__ == "__main__":
     import pandas as pd
@@ -203,7 +173,9 @@ if __name__ == "__main__":
     plt.plot(depth,result)
     plt.show()
 
-    '''クラスの中のinitには一度しか計算が必要ないものを入れる
-    例・窓関数・深さ方向軸・屈折率の記録・参照光のデータ
-    リアルタイム計測に向けて計算は必要最小限に抑える、何度計算しても同じになるものは入れない
-    '''
+'''
+'''
+クラスの中のinitには一度しか計算が必要ないものを入れる
+例・窓関数・深さ方向軸・屈折率の記録・参照光のデータ
+リアルタイム計測に向けて計算は必要最小限に抑える、何度計算しても同じになるものは入れない
+'''
