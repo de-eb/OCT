8863362b2c5987a5a108ddc1b31b35ad0ff9b321
MizobeK <mizobe.kosuke.wa@tut.jp>
2021-12-10 14:38:24 +0900

    眼のモデル

--- Simulator_SD-OCT2.py
+++ Simulator_SD-OCT2.py
@@ -67,11 +67,13 @@ def inverse_ft(freq, itf, xmax, n):
 # Constants
 c0 = 299792458                          # speed of light in vacuum[m/sec]
 n0 = 1.00                               # refractive index of air
-n1 = 1.46                               # refractive index of csample1
-n2 = 1.55                               # refractive index of csample2
+n1 = 1.336                               # refractive index of csample1
+n2 = 1.370                               # refractive index of csample2
+n3 = 1.337                               # refractive index of csample3
 ta = 150e-3                             # thickness of air
-tc1 = 45e-6                             # thickness of sample1
-tc2 = 106e-6                             # thickness of sample2
+tc1 = 10e-6                             # thickness of sample1
+tc2 = 30e-6                             # thickness of sample2
+tc3 = 45e-6                             # thickness of sample3
 
 # Memo : GR...st=200 ed=667 (350~700) WH...st=200 ed=900 (350~860) FL...st=404 ed=613
 st = 200
@@ -90,10 +92,13 @@ R0 = ((n1-n0)/(n0+n1))**2                # reflectance air and sample1
 T0 = 1-R0                                # transmittance
 R1 = ((n2-n1)/(n1+n2))**2                # reflectance sample1 and sample2
 T1 = 1-R1                                # transmittance
-R2 = ((n0-n2)/(n2+n0))**2                # reflectance air and sample2
+R2 = ((n3-n2)/(n2+n3))**2                # reflectance air and sample2
 T2 = 1-R2                                # transmittance
+R3 = ((n0-n3)/(n3+n0))**2                # reflectance air and sample3
+T3 = 1-R3                                # transmittance
 wl_1 = wl/n1                             # wavelength in sample1
 wl_2 = wl/n2                             # wavelength in sample2
+wl_3 = wl/n3                             # wavelength in sample3
 phase_diff = (ta%wl)/wl*2*np.pi+np.pi
 one_cycle = np.arange(0, 1, 1e-3)*2*np.pi
 itf = np.empty_like(wl)
@@ -101,22 +106,24 @@ ref = np.empty_like(wl)
 
 # Calculation the interference
 for i in range(len(wl)):
-    light_ref = 30*R0*sp.values[i]*np.sin(one_cycle+phase_diff.values[i])
+    light_ref = 60*R0*sp.values[i]*np.sin(one_cycle+phase_diff.values[i])
 
     # Light from surface of 1st sample
     light_s1 = sp.values[i]*R0*np.sin(one_cycle+phase_diff.values[i]+np.pi)
 
     # Light throught the 1st sample
     lp1 = (((2*tc1)%wl_1.values[i])/wl_1.values[i])*2*np.pi
-    light1 = sp.values[i]*T0**2*R1*np.sin(one_cycle+phase_diff.values[i]+lp1)*0.7
+    light1 = sp.values[i]*T0**2*R1*np.sin(one_cycle+phase_diff.values[i]+lp1+np.pi)
 
-    # Light from surface of 2nd sample
+    # Light throught the 2nd sample
     lp2 = (((2*tc2)%wl_2.values[i])/wl_2.values[i])*2*np.pi
-    light_s2 = sp.values[i]*(T0**2)*(T1**2)*R2*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2)*0.5
+    light2 = sp.values[i]*(T0**2)*(T1**2)*R2*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2)*0.97
 
-    # check = (light_ref+light_s1+light1)**2                     # sample=1
-    
-    check = (light_ref+light_s1+light1+light_s2)**2     # sample=2
+    # Light throught the 3rd sample
+    lp3 = (((2*tc3)%wl_3.values[i])/wl_3.values[i])*2*np.pi
+    light3 = sp.values[i]*(T0**2)*(T1**2)*(T2**2)*R3*np.sin(one_cycle+phase_diff.values[i]+lp1+lp2+lp3)*0.3
+
+    check = (light_ref+light_s1+light1+light2+light3)**2     # sample=2
     reference = light_ref**2
 
     itf[i] = np.amax(check)
@@ -129,7 +136,7 @@ itf_new = itf - ref
 freq_fixed,itf_fixed = Resampling(wl,itf_new)
 
 # Generate ascan
-depth,result = inverse_ft(freq_fixed*1e-9, itf_fixed, 0.2, n1)
+depth,result = inverse_ft(freq_fixed*1e-9, itf_fixed, 0.1, n1)
 
 # Show graphs
 plt.plot(wl*1e9, sp**2)
