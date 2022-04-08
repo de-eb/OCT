163c3f00410088adc3b01259bdba40cf15c19397
hamasaki <hamasax42193@gmail.com>
2022-04-08 16:26:48 +0900

    該当機器の番号は特定できたが返事が来ない

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -1,4 +1,10 @@
 import pyvisa
+import time
 
 rm=pyvisa.ResourceManager()
-print(rm.list_resources())
+print(rm.list_resources('?*'))
+
+inst=rm.open_resource('USB0::0x1313::0x8087::M00801544::RAW')
+inst.write("*idn?")
+out=inst.read()
+print(out)
