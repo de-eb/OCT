5d4da829564643ba85ffd6f7ac6f22434a2bbdb6
hamasaki <hamasax42193@gmail.com>
2022-04-21 19:27:52 +0900

    ccsのシリアル番号特定は断念、初期化できず

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -1,5 +1,12 @@
 import ctypes
 
-resMgr=None
+instr=ctypes.c_int()
+rscStr=ctypes.c_wchar_p('USB0::0x1313::0x8087::M00801544::RAW')
 
 dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M.dll')
+
+#dev.vi_OpenDefaultResourceManager(ctypes.byref(resMgr))
+#dev.vi_FindResource(resMgr)
+err=dev.tlccs_Init(rscStr,0,0,ctypes.byref(instr))
+
+print(instr,err)
