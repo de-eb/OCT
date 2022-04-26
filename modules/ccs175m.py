57bb50eeb0677eebc03d2f194bf6d92a3862f1f1
hamasaki <hamasax42193@gmail.com>
2022-04-26 17:50:00 +0900

    データの取得方法が今後の課題

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -1,5 +1,6 @@
 import ctypes
 import pyvisa
+import numpy as np
 
 class CcsError(Exception):
     """Base exception describing for this module.
@@ -11,13 +12,13 @@ class CcsError(Exception):
     err: 'int'
         Status codes that VISA driver-level operations can return. 
     """
-    def __init__(self, status_code:int):
+    def __init__(self, status_code:int, session:int):
         self.err=status_code
+        self.inst_handle=session
 
     def __str__(self):
+        dev.tlccs_Close(self.inst_handle)
         return str(pyvisa.constants.StatusCode(self.err))
-
-
 
 #dllファイル　ロード
 dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M.dll')
@@ -27,13 +28,26 @@ rm=pyvisa.ResourceManager()
 inst=rm.open_resource('USB0::0x1313::0x8087::M00801544::RAW')
 inst_handle=inst.session
 
-
-
-integration_time=ctypes.c_double(2.0e-3)
-print(integration_time)
+#露光時間設定？
+integration_time=ctypes.c_double(0.1)
 err=dev.tlccs_SetIntegrationTime(inst_handle,integration_time)
-got_time=ctypes.c_double()
-err=dev.tlccs_GetIntegrationTime(inst_handle,ctypes.byref(got_time))
-print(err,got_time)
-if True:
-    raise CcsError(status_code=-1073807338)
+if err:
+    raise CcsError(status_code=err,session=inst_handle)
+
+
+err=dev.tlccs_StartScan(inst_handle)
+if err:
+    raise CcsError(status_code=err,session=inst_handle)
+
+
+buffer=np.zeros(3694,dtype=np.uint64)
+
+data=ctypes.c_int64()
+err=dev.tlccs_GetScanData(inst_handle,buffer.ctypes.data_as(ctypes.POINTER(ctypes.wintypes.WORD)))
+print(buffer)
+
+
+#セッション終了
+err=tlccs_Close(inst_handle)
+if err:
+    raise CcsError(status_code=err,session=inst_handle)
