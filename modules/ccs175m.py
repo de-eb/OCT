b6d497a58578c60ca226e882ce67bbac9a159249
hamasaki <hamasax42193@gmail.com>
2022-05-13 18:46:48 +0900

    文字列の取り扱い方を習得

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -21,58 +21,65 @@ class CcsError(Exception):
         return str(pyvisa.constants.StatusCode(self.err))
 
 #dllファイル　ロード
-dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M.dll')
+dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M_st4.dll')
 
-#分光器とのセッション確立、セッション番号(アドレス？)の取得
+name='USB0::0x1313::0x8087::M00801544::RAW'
+enc_name=name.encode('utf-8')
+name=ctypes.create_string_buffer(enc_name)
+dev.StringTest(name)
+#dev.StringTest.argtypes=[ctypes.c_wchar_p]
+#print('name = ',name.value)
+#print(dev.StringTest("USB0::0x1313::0x8087::M00801544::RAW"))
+
+'''
+dev.tlccs_Init_Simple.argtypes=(ctypes.c_wchar_p,ctypes.POINTER(ctypes.c_long))
+dev.tlccs_Init_Simple.restype=ctypes.c_long
+
+handle=ctypes.c_long()
+err=dev.tlccs_Init_Simple('USB0::0x1313::0x8087::M00801544::RAW',ctypes.byref(handle))
+print(handle)
+
+
+#分光器とのセッション確立、セッション番号の取得
 rm=pyvisa.ResourceManager()
 inst=rm.open_resource('USB0::0x1313::0x8087::M00801544::RAW')
-inst_handle=inst.session
 
-#露光時間設定？
+
+#露光時間設定
 integration_time=ctypes.c_double(0.1)
-err=dev.tlccs_SetIntegrationTime(inst_handle,integration_time)
+err=dev.tlccs_SetIntegrationTime(inst.session,integration_time)
 if err:
-    raise CcsError(status_code=err,session=inst_handle)
+    raise CcsError(status_code=err,session=inst.session)
 
 
-err=dev.tlccs_StartScan(inst_handle)
+err=dev.tlccs_StartScan(inst.session)
 if err:
-    raise CcsError(status_code=err,session=inst_handle)
+    raise CcsError(status_code=err,session=inst.session)
 
 
 
-buffer=(ctypes.c_double*3648)(0)
-buffer_p=ctypes.pointer(buffer)
+buffer=ctypes.c_double*3648
+buffer3=ctypes.c_double() #double buffer3
+buffer_p=ctypes.POINTER(ctypes.c_double*3648)
 buffer2=np.zeros(3648)
 
 #getscandataに参照渡しする引数はdouble(*?) data[3648];
 
 data=ctypes.c_int64()
+#buffer=dev.GetScanDataArray(inst.session)
 
-dev.tlccs_GetScanData.argtypes=(ctypes.c_int,ctypes.c_double*3648)
-err=dev.tlccs_GetScanData(inst_handle)
+#dev.tlccs_GetScanData.argtypes=(ctypes.c_long,buffer_p)
+dev.tlccs_GetScanData(inst.session,ctypes.byref(buffer))
 
-#<error集>
-#err=dev.tlccs_GetScanData(inst_handle,buffer2.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-#例外が発生しました: ArgumentError
-#argument 2: <class 'TypeError'>: expected c_double_Array_3648 instance instead of LP_c_double
+#err=dev.tlccs_GetScanData(inst.session,buffer2.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
+#dev.GetScanDataArray.restype=(ctypes.c_double)*3648
+#dev.tlccs_GetScanData(inst.session)
 
-#err=dev.tlccs_GetScanData(inst_handle,ctypes.POINTER(buffer))
-#例外が発生しました: TypeError
-#unhashable type
-
-#err=dev.tlccs_GetScanData(inst_handle,buffer)
-#例外が発生しました: OSError
-#exception: access violation reading 0x0000EA18
-
-#err=dev.tlccs_GetScanData(inst_handle,ctypes.byref(buffer))
-#例外が発生しました: ArgumentError
-#argument 2: <class 'TypeError'>: expected c_double_Array_3648 instance instead of pointer to c_double_Array_364
-
-print(buffer)
 
 
 #セッション終了
-err=tlccs_Close(inst_handle)
+
+err=dev.tlccs_Close(inst.session)
 if err:
-    raise CcsError(status_code=err,session=inst_handle)
+    raise CcsError(status_code=err,session=inst.session)
+'''
