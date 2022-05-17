768cc37aeeab021b976639a60e76c37efe77bfe5
hamasaki <hamasax42193@gmail.com>
2022-05-17 14:21:15 +0900

    波長データの取得に成功。波長軸はまだ

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -1,6 +1,6 @@
 import ctypes
-import pyvisa
 import numpy as np
+import matplotlib.pyplot as plt
 
 class CcsError(Exception):
     """Base exception describing for this module.
@@ -21,68 +21,54 @@ class CcsError(Exception):
         return str(pyvisa.constants.StatusCode(self.err))
 
 #dllファイル　ロード
-dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M_st4.dll')
+dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M_gsda9.dll')
 
 name='USB0::0x1313::0x8087::M00801544::RAW'
 enc_name=name.encode('utf-8')
 name=ctypes.create_string_buffer(enc_name)
 handle=ctypes.c_long()
-err=dev.tlccs_Init_Simple(name,ctypes.byref(handle))
-print(handle)
-
-#dev.StringTest.argtypes=[ctypes.c_wchar_p]
-#print('name = ',name.value)
-#print(dev.StringTest("USB0::0x1313::0x8087::M00801544::RAW"))
-
-'''
-dev.tlccs_Init_Simple.argtypes=(ctypes.c_wchar_p,ctypes.POINTER(ctypes.c_long))
-dev.tlccs_Init_Simple.restype=ctypes.c_long
-
-handle=ctypes.c_long()
-err=dev.tlccs_Init_Simple('USB0::0x1313::0x8087::M00801544::RAW',ctypes.byref(handle))
-print(handle)
-
-
-#分光器とのセッション確立、セッション番号の取得
-rm=pyvisa.ResourceManager()
-inst=rm.open_resource('USB0::0x1313::0x8087::M00801544::RAW')
-
+dev.tlccs_Init.argtypes=(ctypes.c_char_p,ctypes.c_bool,ctypes.c_bool,ctypes.POINTER(ctypes.c_long))
+err=dev.tlccs_Init(name,False,False,ctypes.byref(handle))
+if err:
+    raise CcsError(status_code=err,session=handle)
 
 #露光時間設定
-integration_time=ctypes.c_double(0.1)
-err=dev.tlccs_SetIntegrationTime(inst.session,integration_time)
+integration_time=ctypes.c_double(0.01)
+err=dev.tlccs_SetIntegrationTime(handle,integration_time)
 if err:
-    raise CcsError(status_code=err,session=inst.session)
+    raise CcsError(status_code=err,session=handle)
 
 
-err=dev.tlccs_StartScan(inst.session)
+err=dev.tlccs_StartScan(handle)
 if err:
-    raise CcsError(status_code=err,session=inst.session)
+    raise CcsError(status_code=err,session=handle)
 
-
-
-buffer=ctypes.c_double*3648
+ccs_num_pixels=3648 #number of effective pixels of CCD
+buffer=ctypes.c_double*ccs_num_pixels
+buffer2=np.zeros(ccs_num_pixels).astype(np.double)
 buffer3=ctypes.c_double() #double buffer3
-buffer_p=ctypes.POINTER(ctypes.c_double*3648)
-buffer2=np.zeros(3648)
+buffer_p=(ctypes.POINTER(ctypes.c_double)*ccs_num_pixels)()
+buffer_c=ctypes.create_string_buffer(ctypes.sizeof((ctypes.c_double)*ccs_num_pixels))
+buffer_q=np.ctypeslib.as_ctypes(buffer2)
+
 
 #getscandataに参照渡しする引数はdouble(*?) data[3648];
 
-data=ctypes.c_int64()
-#buffer=dev.GetScanDataArray(inst.session)
+
 
 #dev.tlccs_GetScanData.argtypes=(ctypes.c_long,buffer_p)
-dev.tlccs_GetScanData(inst.session,ctypes.byref(buffer))
+#dev.tlccs_GetScanData.argtypes=[ctypes.c_long,ctypes.POINTER(ctypes.c_double)]
+#dev.tlccs_GetScanData(handle,buffer2.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+dev.GetScanDataArray.argtype=(ctypes.c_long)
+dev.GetScanDataArray.restype=np.ctypeslib.ndpointer(dtype=np.double,shape=ccs_num_pixels)
 
-#err=dev.tlccs_GetScanData(inst.session,buffer2.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
-#dev.GetScanDataArray.restype=(ctypes.c_double)*3648
-#dev.tlccs_GetScanData(inst.session)
-
+data=dev.GetScanDataArray(handle)
+plt.plot(data)
+plt.show()
 
 
 #セッション終了
 
-err=dev.tlccs_Close(inst.session)
+err=dev.tlccs_Close(handle)
 if err:
-    raise CcsError(status_code=err,session=inst.session)
-'''
+    raise CcsError(status_code=err,session=handle)
