1e2e0ffdef976004d98d3aaecb16d62c503b2d19
hamasaki <hamasax42193@gmail.com>
2022-05-18 17:36:32 +0900

    class Ccs175m を作成(未完成)

--- modules/ccs175m.py
+++ modules/ccs175m.py
@@ -21,13 +21,97 @@ class CcsError(Exception):
         dev.tlccs_Close(self.inst_handle)
         return str(pyvisa.constants.StatusCode(self.err))
 
+class Ccs175m():
+    """Class to control compact spectrometer(CCS175/M)
+    """
+    #External modules loading
+    __dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M.dll')
+
+    num_pixels=3648 #number of effective pixels of CCD
+
+    def __init__(self,name:str):
+        """Initiates and unlock communication with the device
+
+        Parameters
+        ----------
+        name : `str`, required
+            VISA resource name of the device.
+
+        Raise
+        ---------
+        CcsError :
+            When the module initialization fails.
+        """
+        #device initializing
+        self.name=ctypes.create_string_buffer(name.encode('utf-8'))
+        self.handle=ctypes.c_long()
+        Ccs175m.__dev.tlccs_Init.argtypes=(ctypes.c_char_p,ctypes.c_bool,ctypes.c_bool,ctypes.POINTER(ctypes.c_long))
+        err=Ccs175m.__dev.tlccs_Init(self.name,False,False,ctypes.byref(self.handle))
+        if err:
+            raise CcsError(status_code=err,session=handle)
+
+        #get wavelength data
+        Ccs175m.__dev.GetWavelengthDataArray.argtype=(ctypes.c_long)
+        Ccs175m.__dev.GetWavelengthDataArray.restype=np.ctypeslib.ndpointer(dtype=np.double,shape=Ccs175m.num_pixels)
+        self.__wavelength=Ccs175m.__dev.GetWavelengthDataArray(handle)
+
+        #Set types of arguments and return value of the GetScanDataArray function.
+        Ccs175m.__dev.GetScanDataArray.argtypes=(ctypes.c_long)
+        Ccs175m.__dev.GetScanDataArray.restypes=np.ctypeslib.ndpointer(dtype=np.double,shape=Ccs175m.num_pixels)
+    
+    @property
+    def wavelength(self):
+        """Wavelength [nm] axis corresponding to the measurement data.
+        """
+        return self.__wavelength
+    
+    def set_IntegrationTime(self,time=0.01):
+        """This function set the optical integration time in seconds.
+
+        Parameter
+        ---------
+        time : `float`
+            The optical integration time.
+        
+        Raise
+        ---------
+        CcsError :
+            When the setting fails.
+        """
+        err=Ccs175m.__dev.tlccs_SetIntegrationTime(self.handle)
+        if err:
+            raise CcsError(status_code=err,session=handle)
+
+    def read_spectra(self,averaging=1):
+        """This function triggers the CCS to take one single scan 
+        and reads out the processed scan data.
+        """
+        Ccs175m.__dev.tlccs_SrartScanCont(handle)
+        data=np.zeros()
+
+
+
+    def close(self):
+        """This function close an instrument driver session.
+        
+        Raise
+        --------
+        CcsError :
+            When the module is not controlled correctly.
+        """
+        err=Ccs175m.__dev.tlccs_Close(self.handle)
+        if err:
+            raise CcsError(status_code=err,session=handle)
+        
+
+
+
 #dllファイル　ロード
 dev=ctypes.windll.LoadLibrary(r'modules\tools\CCS175M.dll')
 
 #接続
 name='USB0::0x1313::0x8087::M00801544::RAW'
-enc_name=name.encode('utf-8')
-name=ctypes.create_string_buffer(enc_name)
+name=ctypes.create_string_buffer(name.encode('utf-8'))
 handle=ctypes.c_long()
 dev.tlccs_Init.argtypes=(ctypes.c_char_p,ctypes.c_bool,ctypes.c_bool,ctypes.POINTER(ctypes.c_long))
 err=dev.tlccs_Init(name,False,False,ctypes.byref(handle))
@@ -58,6 +142,8 @@ wl=dev.GetWavelengthDataArray(handle)
 
 #データプロット
 plt.plot(wl,sp)
+plt.xlabel("wavelength[nm]",fontsize=17)
+plt.ylabel("light intensity[arb. unit]",fontsize=17)
 plt.show()
 
 #セッション終了
