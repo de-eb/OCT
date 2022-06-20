d9d6b794c01ad3f814f2cb5a1fd8b6f481c112c4
hamasaki <hamasax42193@gmail.com>
2022-06-20 14:38:46 +0900

    絶対位置駆動コマンドに成功

--- modules/crux.py
+++ modules/crux.py
@@ -4,4 +4,18 @@ ser=serial.Serial(
     port='COM4'
 )
 
-info={}
+def decoder(command,param):
+    result='\2'+command
+    for i in range(len(param)):
+        result=result+str(param[i])
+        if i+1 != len(param):
+            result=result+'/'
+    result=result+'\r\n'
+    print(result)
+    return result.encode('ascii')
+
+move=decoder('APS',[1,0,1000000,0])
+
+ser.write(move)
+result=ser.readline()
+print(result)
