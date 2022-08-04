3aaa1daad3abdb607926d88f05e58d7d104b205f
hamasax42193 <hamasx42193@gmail.com>
2022-08-04 12:09:39 +0900

    vmaxの追加

--- modules/2d-image_generator.py
+++ modules/2d-image_generator.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 if __name__=="__main__":
     #constants
-    filename='data/220802_1.csv'
+    filename='data/220803_1.csv'
     resolution=1500
     n=1.5
-    depth_max=0.4
+    depth_max=0.3
     resolution=1500
-    width_h=20
+    width_h=10
     aspect=2/3
 
     data=dh.load_spectra(file_path=filename,wavelength_range=[770,910])
@@ -21,7 +21,7 @@ if __name__=="__main__":
     for i in tqdm(range(len(data['spectra'][0]))):
         result_map[i]=sp.generate_ascan(data['spectra'][:,i], data['reference'])
     plt.figure()
-    plt.imshow(result_map,cmap='gray',extent=[0,depth_max,0,width_h],aspect=(depth_max/width_h)*aspect)
+    plt.imshow(result_map,cmap='gray',extent=[0,depth_max,0,width_h],aspect=(depth_max/width_h)*aspect,vmax=0.3)
     plt.xlabel('depth[mm]')
     plt.ylabel('width[mm]')
     plt.show()
