05da88d529a0979c839494704664e4904b42c3bb
hamasax42193 <hamasx42193@gmail.com>
2022-10-17 17:28:02 +0900

    計算結果保存・読み出し機能を追加

--- modules/cross_section_generator.py
+++ modules/cross_section_generator.py
@@ -2,6 +2,7 @@ import numpy as np
 from signal_processing_hamasaki import SignalProcessorHamasaki as Processor
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import glob
 
 def find_index(depth,target)->int:
     array=depth*1e3
@@ -10,11 +11,17 @@ def find_index(depth,target)->int:
         return 0
     else:
         for i in range(len(array)):
-            print(array[i])
             if array[i]>=target:
                 index=i
                 break
         return index
+
+def generate_CrossSection(data,index):
+    result=np.zeros((len(data[0]),len(data)))
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            result[i][j]=data[i][j][index]
+    return result
 
 if __name__== "__main__":
     #constants
@@ -24,25 +31,43 @@ if __name__== "__main__":
     n=1.5
     st=1664
     ed=2491
-    target_depth=140
+    target_depth=205
 
-    #data loading
-    data=np.load(filename,allow_pickle=True)
-    sp=Processor(data['wavelength'][st:ed],n,depth_max,resolution)
-    result_all=np.zeros((len(data['spectra']),len(data['spectra'][0]),resolution))
-    print('<data information>\ndate:{}\nmemo:{}'.format(data['date'][0],data['memo'][0]))
-    #signal processing
-    for i in tqdm(range(len(data['spectra']))):
-        for j in range(len(data['spectra'][0])):
-            result_all[i][j]=sp.generate_ascan(data['spectra'][i][j][st:ed], data['reference'][st:ed])
-    
-    cs_map=np.zeros((len(data['spectra'][0]),len(data['spectra'])))
-    target_index=find_index(sp.depth,target_depth)
-    print(target_index)
-    for i in range(len(cs_map)):
-        for j in range(len(cs_map[i])):
-            print(result_all[i][j][target_index])
-            cs_map[i][j]=result_all[i][j][target_index]
-    
+    process_flag=False
+
+    if glob.glob(pathname=filename.strip('.npz')+'_calculated.npz'):
+        print("Calculated file confirmed.")
+        founddata=np.load(file=filename.strip('.npz')+'_calculated.npz',allow_pickle=True)
+        if founddata['depth_max'][0]==depth_max \
+        and founddata['resolution'][0]==resolution \
+        and founddata['n'][0]==n:
+            print('Calculation conditions matched.')
+            print('<data information>\ndate:{}\nmemo:{}'.format(founddata['date'][0],founddata['memo'][0]))
+            target_index=find_index(founddata['depth'], target_depth)
+            cs_map=generate_CrossSection(founddata['data'],target_index)
+            process_flag=True
+        else:
+            print('Calculation condition did not matched.')
+    if process_flag is not True:
+        data=np.load(filename,allow_pickle=True)
+        sp=Processor(data['wavelength'][st:ed],n,depth_max,resolution)
+        result_all=np.zeros((len(data['spectra']),len(data['spectra'][0]),resolution))
+        print('<data information>\ndate:{}\nmemo:{}'.format(data['date'][0],data['memo'][0]))
+        for i in tqdm(range(len(data['spectra']))):
+            for j in range(len(data['spectra'][0])):
+                result_all[i][j]=sp.generate_ascan(data['spectra'][i][j][st:ed], data['reference'][st:ed])
+        target_index=find_index(sp.depth,target_depth)
+        cs_map=generate_CrossSection(result_all, target_index)
+
+        np.savez_compressed(filename.strip('.npz')+'_calculated.npz',
+        data=result_all,
+        date=data['date'],
+        memo=data['memo'],
+        resolution=np.array([resolution],dtype=int),
+        n=np.array([n],dtype=float),
+        depth_max=np.array([depth_max],dtype=float),
+        depth=sp.depth
+        )
+
     plt.imshow(cs_map)
     plt.show()
