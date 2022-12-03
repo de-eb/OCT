f8189eac9dd390c805b0b4123f89c3b7d0f470f0
hamasax42193 <hamasx42193@gmail.com>
2022-12-03 16:18:58 +0900

    測定・計算結果のグラフ表示までできるようになった(保存機能はまだ)

--- sd-oct.py
+++ sd-oct.py
@@ -12,6 +12,7 @@ from modules.fine01r import Fine01r, Fine01rError
 from modules.crux import Crux,CruxError
 from modules.artcam130mi import ArtCam130
 from modules.signal_processing_hamasaki import SignalProcessorHamasaki as Processor
+from modules.signal_processing_hamasaki import calculate_absorbance 
 import modules.data_handler as dh
 from modules.ccs175m import Ccs175m,CcsError
 
@@ -75,7 +76,7 @@ if __name__ == "__main__":
     ccs_ed=2491 # Calculation range (End) of spectrum(ccs)
     pl_rate=2000 # Number of pulses equals to 1mm [pulse/mm]
     pma_st=134 # calculation range(start) of spectrum(pma)
-    pma_ed=938 # calculation range(end) of spectrum(pma)
+    pma_ed=953 # calculation range(end) of spectrum(pma)
 
     #Flag for equipment operation
     stage_s_flag=None #sample stage(Crux)
@@ -86,15 +87,26 @@ if __name__ == "__main__":
     vi=0 #initial position of vertical stage
     hi=0 #initial position of horizontal stage
 
-    # Device settings
-    try: stage_m = Fine01r('COM11')  # Piezo stage (reference mirror side)
+    # Device connection
+    ccs=Ccs175m(name='USB0::0x1313::0x8087::M00801544::RAW') #Spectrometer (for OCT measurement)
+    # Spectrometer (for Absorbance measurement)
+    try: pma = Pma12(dev_id=5)  
+    except PmaError:
+        print('\033[31m'+'Error:PMA not found. Absorbance measurement function is disabled.'+'\033[0m ')
+        stage_p_flag=False
+    else:
+        stage_p_flag=True
+        
+    # Piezo stage (reference mirror side)
+    try: stage_m = Fine01r('COM11')  
     except Fine01rError:
         print('\033[31m'+'Error:FINE01R not found. Reference mirror movement function is disabled.'+'\033[0m ')
         stage_m_flag=False
     else:
         stage_m_flag=True
     
-    try: stage_s = Crux('COM4')  # Auto stage (sample side)
+    # Auto stage (sample side)
+    try: stage_s = Crux('COM4')  
     except CruxError:
         print('\033[31m'+'Error:Crux not found. Sample stage movement function is disabled.'+'\033[0m ')
         stage_s_flag=False
@@ -107,14 +119,6 @@ if __name__ == "__main__":
             print('Stage position data loaded.')
             stage_s.biaxial_move(v=vi, vmode='a', h=hi, hmode='a')
 
-    try: pma = Pma12(dev_id=5)  # Spectrometer (old)
-    except PmaError:
-        print('\033[31m'+'Error:PMA not found. Absorbance measurement function is disabled.'+'\033[0m ')
-        stage_p_flag=False
-    else:
-        stage_p_flag=True
-
-    ccs=Ccs175m(name='USB0::0x1313::0x8087::M00801544::RAW') #Spectrometer (for OCT measurement)
     sp = Processor(ccs.wavelength[ccs_st:ccs_ed], n=1.5,depth_max=depth_max,resolution=resolution)
     q = Queue()
     proc1 = Process(target=profile_beam, args=(q,))  # Beam profiler
@@ -127,12 +131,13 @@ if __name__ == "__main__":
     ascan = np.zeros_like(sp.depth)
     
     #Array for Absorbance calculation
-    inc = None # incident light spectra
     reflect = np.zeros((step_h,pma.wavelength.size),dtype=float) #refrected light
+    inc=None
     absorbance=np.zeros(pma_ed-pma_st)
 
     #Variables for others
-    err = False
+    ccs_err = False
+    pma_err = False
     location=np.zeros(3,dtype=int) 
     x, y, z = 100000, 0, 0  # Stage position (Initial)
 
@@ -164,8 +169,8 @@ if __name__ == "__main__":
     ax2 = fig.add_subplot(222,title='Spectrometer output(PMA)',xlabel='Wavelength [nm]',ylabel='Intensity [-]')
     ax2.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
     ax2.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
-    ax2_0,=ax2.plot(pma.wavelength[pma_st:pma_ed], reflect[0,pma_st:pma_ed], label='reflection')
-    ax2_1,=ax2.plot(pma.wavelength[pma_st:pma_ed], reflect[0,pma_st:pma_ed], label='incidence')
+    ax2_0,=ax2.plot(pma.wavelength[pma_st:pma_ed], reflect[0,pma_st:pma_ed]+1, label='reflection')
+    ax2_1,=ax2.plot(pma.wavelength[pma_st:pma_ed], reflect[0,pma_st:pma_ed]+1, label='incidence')
     ax2.legend(bbox_to_anchor=(1,1), loc='upper right', borderaxespad=0.2)
     ax2.set_yscale("log")
 
@@ -173,12 +178,12 @@ if __name__ == "__main__":
     ax3 = fig.add_subplot(224,title='Absorbance', xlabel='Wavelength [nm]')
     ax3.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
     ax3.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
-    ax3_0,=ax3.plot(pma.wavelength[pma_st:pma_ed],absorbance, label='reflection')
+    ax3_0,=ax3.plot(pma.wavelength[pma_st:pma_ed],absorbance)
 
     # Device initialization
     if stage_m_flag:
         stage_m.absolute_move(z)
-    #pma.set_parameter(shutter=1)
+    pma.set_parameter(shutter=1)
     ccs.set_IntegrationTime(time=0.0001)
     ccs.start_scan()
 
@@ -198,19 +203,19 @@ if __name__ == "__main__":
             location[1]=stage_s.read_position(axis_num=2)
             print('Stage position:x={}[mm],y={}[mm],z={}[nm]'.format((location[0]-hi)/pl_rate,(location[1]-vi)/pl_rate,location[2]/pl_rate))
 
-        # Spectral measurement
+        # Spectral measurement (CCS)
         try: itf[0,:] = ccs.read_spectra(averaging=5)
-        except CcsError as e:
-            err = True
-            print(e, end="\r")
+        except CcsError as ccs_e:
+            ccs_err = True
+            print('\033[31m'+'CcsError:'+'\033[0m '+ ccs_e, end="\r")
         else:
-            if err:
+            if ccs_err:
                 print("                            ", end="\r")
-                err= False
+                ccs_err= False
         ax0_0.set_data(ccs.wavelength[ccs_st:ccs_ed], itf[0,ccs_st:ccs_ed])  # Graph update
         ax0.set_ylim((0, 1.2*itf[0,ccs_st:ccs_ed].max()))
 
-        # Signal processing
+        # Signal processing　and plot(CCS)
         if reference is not None:
             ascan = sp.generate_ascan(itf[0,ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
             if use_um:# Graph update
@@ -219,12 +224,37 @@ if __name__ == "__main__":
                 ax1_0.set_data(sp.depth, ascan)
             ax1.set_ylim((0,np.amax(ascan)))
 
+        #Spectral measurement (PMA)
+        try: reflect[0,:] = pma.read_spectra(averaging=5)
+        except PmaError as pma_e:
+            pma_err = True
+            print('\033[31m'+'PmaError:'+'\033[0m '+ pma_e, end="\r")
+        else:
+            if pma_err:
+                print("                            ", end="\r")
+                pma_err= False                   
+        ax2_0.set_data(pma.wavelength[pma_st:pma_ed],reflect[0,pma_st:pma_ed])
+        
+        #Signal processing and plot(PMA)
+        if inc is None:
+            ax2.set_ylim((1,np.amax(reflect[0,pma_st:pma_ed])*1.2))
+        else:
+            absorbance=calculate_absorbance(reflect[0,pma_st:pma_ed], inc[pma_st:pma_ed])
+            ax3_0.set_data(pma.wavelength[pma_st:pma_ed],absorbance)
+            ax3.set_ylim(0,np.nanmax(absorbance))
+
+        if g_key == 'a':
+            inc = pma.read_spectra(averaging=50)
+            print('Incident light data updated.')
+            ax2_1.set_data(pma.wavelength[pma_st:pma_ed],inc[pma_st:pma_ed])
+            ax2.set_ylim(top=np.amax(inc)*1.2)
+
         #'Delete' key to delete reference and a-scan data       
         if g_key=='delete':
             reference=None
             ax0_1.set_data(ccs.wavelength[ccs_st:ccs_ed],np.zeros(ed-st))
             ax1_0.set_data(sp.depth*1e3,np.zeros_like(sp.depth))
-            print('Reference data deleted.')            
+            print('Reference data deleted.')        
 
         # 'Enter' key to update reference data
         if g_key == 'enter':
