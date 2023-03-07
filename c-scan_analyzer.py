7dd221cc579e4154c2ac7df0775ff839bc4d7ca5
hamasax42193 <hamasax42193@gmail.com>
2023-03-07 20:09:13 +0900

    機能を統合したc-scan_analyzer.pyを追加

--- c-scan_analyzer.py
+++ c-scan_analyzer.py
@@ -1,25 +1,25 @@
 import numpy as np
-from signal_processing_hamasaki import SignalProcessorHamasaki as Processor
-from signal_processing_hamasaki import generate_tomographical_view
+from modules.signal_processing_hamasaki import SignalProcessorHamasaki as Processor
 import matplotlib.pyplot as plt
-import data_handler as dh
+import modules.data_handler as dh
 import glob
 
 if __name__=="__main__":
-    #constants
+    #calculate conditions
     filename='data/thin_skin_of_onion.npz'
     resolution=2000
     depth_max=0.3
     n=1.5
-    st=1664
-    ed=2491
-    target=0.2
-    vmax=0.008
-    w_or_h='w'
+    wl_start, wl_end=770, 910
+
+    #graph setting
+    target=0.15 #[mm]
+    vmax=1
     aspect=1
+    mode='xy' #'xd' or 'yd' or 'xy' only
+    colormap='gray' #choose from https://matplotlib.org/stable/tutorials/colors/colormaps.html
 
     data_prepared=False
-
     if glob.glob(filename.strip('.npz')+'_calculated.npz'): 
         print("Calculated file confirmed.")
         c_data=np.load(file=filename.strip('.npz')+'_calculated.npz',allow_pickle=True,mmap_mode='r')
@@ -34,6 +34,7 @@ if __name__=="__main__":
 
     if data_prepared is False:
         data=np.load(file=filename,allow_pickle=True,mmap_mode='r')
+        st,ed=Processor.find_index(data['wavelength'],[wl_start,wl_end])
         sp=Processor(data['wavelength'][st:ed],n,depth_max,resolution)
         dh.output_datainfo(data)
         result_all=sp.generate_cscan(data['spectra'][:,:,st:ed],data['reference'][st:ed])
@@ -51,20 +52,27 @@ if __name__=="__main__":
         print('Calculation result saved.')
         c_data=np.load(filename.strip('.npz')+'_calculated.npz',allow_pickle=True,mmap_mode='r')
     
-    plt.figure()
-    plt.xlabel('Depth [μm]',fontsize=13)
     
+    plt.figure()
+    plt.rcParams["figure.figsize"] = (6, 6)
     plt.xticks(fontsize=13)
     plt.yticks(fontsize=13)
-    if w_or_h == 'w':
-        plt.ylabel('X [mm]',fontsize=13)
-        result=generate_tomographical_view(c_data['data'],c_data['width'][0],target,w_or_h)
-        plt.imshow(result,cmap='gray',extent=[0,depth_max*1e3,0,c_data['width'][0]],aspect=(depth_max*1e3/c_data['width'][0])*aspect,vmax=vmax)
+    if mode == 'xd':
+        plt.xlabel('Depth [mm]',fontsize=15)
+        plt.ylabel('X [mm]',fontsize=15)
+        result=Processor.analyze_cscan(cscan=c_data['data'],target=target,mode=mode,y_max=c_data['width'][0])
+        plt.imshow(result,cmap=colormap,extent=[0,depth_max,0,c_data['width'][0]],aspect=aspect,vmax=np.amax(result)*vmax)
 
-    elif w_or_h == 'h':
-        plt.ylabel('Y [mm]',fontsize=13)
-        result=generate_tomographical_view(c_data['data'],c_data['height'][0],target,w_or_h)
-        plt.imshow(result,cmap='gray',extent=[0,depth_max*1e3,0,c_data['height'][0]],aspect=(depth_max*1e3/c_data['height'][0])*aspect,vmax=vmax)
+    elif mode == 'yd':
+        plt.xlabel('Depth [mm]',fontsize=15)
+        plt.ylabel('Y [mm]',fontsize=15)
+        result=Processor.analyze_cscan(cscan=c_data['data'],target=target,mode=mode,y_max=c_data['height'][0])
+        plt.imshow(result,cmap=colormap,extent=[0,depth_max,0,c_data['height'][0]],aspect=aspect,vmax=np.amax(result)*vmax)
 
-    plt.vlines(260,0,c_data['width'],colors='red')
+    elif mode=='xy':
+        plt.ylabel('X [mm]',fontsize=15)
+        plt.ylabel('Y [mm]',fontsize=15)        
+        result=Processor.analyze_cscan(cscan=c_data['data'],target=target,mode=mode,depth=c_data['depth'])
+        plt.imshow(result,cmap=colormap,extent=[0,c_data['width'][0],0,c_data['height'][0]],aspect=aspect,vmax=np.amax(result)*vmax)
+
     plt.show()
