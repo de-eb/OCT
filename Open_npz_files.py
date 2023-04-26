c3102e0773f28f1bbf76380104310eb99a4c2d37
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-04-26 16:23:38 +0900

    npzファイルの中身が見れます

---
+++ Open_npz_files.py
@@ -0,0 +1,17 @@
+import numpy as np
+from modules.signal_processing_hamasaki import SignalProcessorHamasaki as Processor
+import matplotlib.pyplot as plt
+
+
+filename = 'data/thin_skin_of_onion.npz'
+data = np.load(file = filename, allow_pickle=True)
+
+
+#npzファイルの中身
+print("npzファイルに含まれている各要素\n", data.files)
+
+for i in range(len(data.files)):
+    print(data.files[i])
+    name = data.files[i]
+    print("%s" % data[name])
+
