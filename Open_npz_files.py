656956f3988e1d9ed4471da24cb9f39e1db738df
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-04-27 11:51:44 +0900

    更新しました

--- Open_npz_files.py
+++ Open_npz_files.py
@@ -11,7 +11,8 @@ data = np.load(file = filename, allow_pickle=True)
 print("npzファイルに含まれている各要素\n", data.files)
 
 for i in range(len(data.files)):
-    print(data.files[i])
+    print(data.files[i], end = " ")
+    # print(" 要素数:", len(data.files[i]))
     name = data.files[i]
     print("%s" % data[name])
 
