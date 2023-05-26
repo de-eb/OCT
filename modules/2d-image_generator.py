50e6755f917ae790635c2cf435a01b49b39c6b7f
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-05-26 11:13:39 +0900

    カラーバーにvminを追加しました

--- modules/2d-image_generator.py
+++ modules/2d-image_generator.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 
 if __name__=="__main__":
     #constants
-    filename='data/wet_tissue_paper.csv'
+    filename='data/2layer_cellophane_0508.csv'
     resolution=4000
     n=1.5
-    depth_max=0.275
+    depth_max=0.3
     width_h=1
     aspect=1
-    vmax=0.02
+    vmin=0.05
+    vmax=0.25
 
     data=dh.load_spectra(file_path=filename,wavelength_range=[770,910])
     print('<data information>\nfilename:{}\ndate:{}\nmemo:{}'.format(filename,data['date'],data['memo']))
@@ -20,7 +21,7 @@ if __name__=="__main__":
     result_map=sp.generate_bscan(data['spectra'], data['reference'])
 
     plt.figure()
-    plt.imshow(result_map,cmap='gray',extent=[0,depth_max*1e3,0,width_h],aspect=(depth_max*1e3/width_h)*aspect,vmax=np.amax(result_map)*vmax)
+    plt.imshow(result_map,cmap='gray',extent=[0,depth_max*1e3,0,width_h],aspect=(depth_max*1e3/width_h)*aspect,vmin=vmin,vmax=np.amax(result_map)*vmax)
     plt.xlabel('Depth [µm]',fontsize=14)
     plt.ylabel('X [mm]',fontsize=14)
     plt.xticks(fontsize=14)
