0a74592f2a13af9a7f0b13fd61ab0ae12a22396a
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-06-14 11:34:14 +0900

    グラフ周辺の変更

--- modules/2d-image_generator.py
+++ modules/2d-image_generator.py
@@ -1,30 +1,29 @@
-from signal_processing_hamasaki import SignalProcessorHamasaki as Processor
+from signal_processing_mizobe import SignalProcessorMizobe as Processor
 import data_handler as dh
-from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 if __name__=="__main__":
     #constants
-    filename='data/2layer_cellophane_0508.csv'
-    resolution=4000
-    n=1.5
-    depth_max=0.3
-    width_h=1
-    aspect=1
-    vmin=0.05
-    vmax=0.25
+    filename = 'data/2layer_cellophane_0508.csv'
+    resolution = 4000
+    n = 1.50
+    depth_max = 0.3
+    width_h = 1
+    aspect = 1
+    vmin = 0.05
+    vmax = 0.25
 
-    data=dh.load_spectra(file_path=filename,wavelength_range=[770,910])
-    print('<data information>\nfilename:{}\ndate:{}\nmemo:{}'.format(filename,data['date'],data['memo']))
-    sp=Processor(data['wavelength'], n, depth_max, resolution)
-    result_map=sp.generate_bscan(data['spectra'], data['reference'])
+    data = dh.load_spectra(file_path = filename,wavelength_range = [770,910])
+    print('<data information>\n filename:{}\n date:{}\n memo:{}'.format(filename,data['date'],data['memo']))
+    sp = Processor(data['wavelength'], n, depth_max, resolution)
+    b_scan = sp.generate_bscan(data['spectra'], data['reference'])
 
     plt.figure()
-    plt.imshow(result_map,cmap='gray',extent=[0,depth_max*1e3,0,width_h],aspect=(depth_max*1e3/width_h)*aspect,vmin=vmin,vmax=np.amax(result_map)*vmax)
-    plt.xlabel('Depth [µm]',fontsize=14)
-    plt.ylabel('X [mm]',fontsize=14)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-
+    plt.imshow(b_scan,cmap = 'gist_gray',extent = [0,depth_max*1e3,0,width_h],aspect = (depth_max*1e3/width_h)*aspect,vmin = vmin,vmax = np.amax(b_scan)*vmax)
+    plt.xlabel('Depth [µm]',fontsize = 14)
+    plt.ylabel('X [mm]',fontsize = 14)
+    plt.xticks(fontsize = 14)
+    plt.yticks(fontsize = 14)
     plt.show()
