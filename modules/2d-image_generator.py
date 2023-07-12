aa110d6c2445e91191c546e687c47beddda47adf
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-07-12 18:19:31 +0900

    フォントとx軸の範囲、レイアウトを変更

--- modules/2d-image_generator.py
+++ modules/2d-image_generator.py
@@ -5,12 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 14
+
 if __name__=="__main__":
     # 初期設定(OCT)
-    filename_ccs = 'data/230710_RGC_focus_x6.csv'
-    n , resolution , depth_max , width = 1.51 , 3000 , 0.5 , 10
-    extent , aspect = [0, depth_max*1e3, 0, width] , (depth_max*1e3/width)*1              # aspect : 1の値を変えて調整可能
-    vmin_oct , vmax_oct = 0.01 , 0.25
+    filename_ccs = 'data/230711_RGC_150_x6(new).csv'
+    n , resolution , depth_max , width = 1.51 , 3000 , 0.5 , 10.0
+    extent_oct , aspect_oct = [0, depth_max*1e3, 0, width] , (depth_max*1e3/width)*1              # aspect : 1の値を変えて調整可能
+    vmin_oct , vmax_oct = 0.00 , 0.26
     
     # データ読み込み
     data_ccs = dh.load_spectra(file_path = filename_ccs, wavelength_range = [770, 910])
@@ -18,11 +21,18 @@ if __name__=="__main__":
     sp = Processor(data_ccs['wavelength'], n, depth_max, resolution)
     b_scan = sp.generate_bscan(data_ccs['spectra'], data_ccs['reference'])
     
-    
+    # グラフ表示(B-scan)
+    plt.figure(tight_layout = True)
+    plt.imshow(b_scan, cmap = 'jet', extent = extent_oct, aspect = aspect_oct, vmin = vmin_oct, vmax = np.amax(b_scan)*vmax_oct)
+    plt.colorbar()
+    plt.xlabel('Depth [µm]')
+    plt.ylabel('Width [mm]')
+    plt.show()
+
     # 初期設定(SS)
-    filename_pma = 'data/230710_SS_RGC_x7_Av10.csv'
-    width = 10.0                                                                          # 水平方向、垂直方向の走査幅 [mm]
-    st , ed , vmin_ss, vmax_ss = 201 , 940 , 0.00 , 3.0                                   # スペクトル（CCS）の計算範囲
+    filename_pma = 'data/230710_RGC_SS_x7_Av10.csv'
+    width , st , ed = 10.0 , 201 , 940                               # 水平方向、垂直方向の走査幅 [mm]
+    vmin_ss , vmax_ss = 0.00 , 3.0
     
     # データ読み込み
     data_pma = dh.load_spectra(file_path = filename_pma, wavelength_range = [201,941])
@@ -30,24 +40,33 @@ if __name__=="__main__":
     wavelength, reflect, incident = data_pma['wavelength'], data_pma['spectra'], data_pma['reference']
     reflectance = calculate_reflectance_2d(reflection = reflect, incidence = incident)
     pma_st, pma_ed = Processor.find_index(wavelength, [st, ed])
+    extent_ss , aspect_ss = [wavelength[pma_st], wavelength[pma_ed], 0, width] , ((abs(wavelength[pma_st] - wavelength[pma_ed]) / width))*(2/3)
 
-    
-
-    # グラフ表示(B-scan)
-    plt.imshow(b_scan, cmap = 'jet', extent = extent, aspect = aspect, vmin = vmin_oct, vmax = np.amax(b_scan)*vmax_oct)    # cmapは jet or gist_gray
+    # グラフ表示(SS)
+    plt.figure(tight_layout = True)
+    plt.imshow(reflectance, cmap = 'jet', extent = extent_ss, aspect = aspect_ss, vmin = vmin_ss, vmax = vmax_ss)
     plt.colorbar()
-    plt.xlabel('Depth [µm]', fontsize = 12)
-    plt.ylabel('Width [mm]', fontsize = 12)
+    plt.xlabel('Wavelength [nm]')
+    plt.ylabel('Width [mm]')
+    plt.xlim(300, 900)
     plt.show()
-    
 
-    # グラフ表示(ss)
-    # plt.imshow(reflectance, cmap = 'jet', extent = extent, aspect = aspect, vmin = vmin, vmax = vmax)
-    # plt.colorbar()
-    # plt.xlabel('Wavelength [nm]', fontsize = 12)
-    # plt.ylabel('Width [mm]', fontsize = 12)
-    # plt.show()
     
+    # # グラフ表示(B-scan & SS)
+    # plt.figure(figsize = (12,5), tight_layout = True)
+    # plt.subplot(121, title = 'B-scan')
+    # plt.imshow(b_scan, cmap = 'jet', extent = extent_oct, aspect = aspect_oct, vmin = vmin_oct, vmax = np.amax(b_scan)*vmax_oct)
+    # plt.colorbar(shrink = 0.45)
+    # plt.xlabel('Depth [µm]')
+    # plt.ylabel('Width [mm]')
+
+    # plt.subplot(122, title = 'Reflectance')
+    # plt.imshow(reflectance, cmap = 'jet', extent = extent_ss, aspect = aspect_ss, vmin = vmin_ss, vmax = vmax_ss)
+    # plt.colorbar(shrink = 0.45)
+    # plt.xlabel('Wavelength [nm]')
+    # plt.ylabel('Width [mm]')
+    # plt.show()
+
     
     """
     # 特定の深さにおけるA-scan
