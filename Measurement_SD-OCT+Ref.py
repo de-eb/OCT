5820f1f3f77f56d1f2e3ba733c4c0ed5abf17da7
MizobeK <mizobe.kosuke.wa@tut.jp>
2023-07-19 14:48:51 +0900

    OCTと反射率の同時測定に対応

--- Measurement_SD-OCT+Ref.py
+++ Measurement_SD-OCT+Ref.py
@@ -134,11 +134,11 @@ if __name__ == "__main__":
     ax0.legend(bbox_to_anchor = (1,1), loc = 'upper right', borderaxespad = 0.2)
 
     if use_um:
-        ax1 = fig.add_subplot(223, title = 'A-scan', xlabel = 'depth [μm]', ylabel = 'Intensity [-]')
+        ax1 = fig.add_subplot(223, title = 'A-scan', xlabel = 'Depth [μm]', ylabel = 'Intensity [-]')
         ax1_0, = ax1.plot(sp.depth*1e3, ascan)
         ax1.set_xlim(0,np.amax(sp.depth)*1e3)
     else:
-        ax1 = fig.add_subplot(223, title = 'A-scan', xlabel = 'depth [mm]', ylabel = 'Intensity [-]')
+        ax1 = fig.add_subplot(223, title = 'A-scan', xlabel = 'Depth [mm]', ylabel = 'Intensity [-]')
         ax1_0, = ax1.plot(sp.depth, ascan)
         ax1.set_xlim(0,np.amax(sp.depth))
     ax1.yaxis.set_major_formatter(ScalarFormatter(useMathText = True))
@@ -172,38 +172,36 @@ if __name__ == "__main__":
     # メインループ
     while g_key != 'escape':
 
+
         """ ステージの操作コマンド一覧 """
-        # 自動ステージ（試料ホルダー）の位置調整
-        if g_key in ['4','6','5','2','8']:
-            if g_key == '6':stage_s.relative_move(2000, axis_num = 1, velocity = 9)           # ６：右方向に1mm移動
-            elif g_key == '4':stage_s.relative_move(-2000, axis_num = 1, velocity = 9)        # ４：左方向に1mm移動
-            elif g_key == '5':                                                                # ５：ステージの初期位置に移動
+        if g_key in ['4','6','5','2','8']:                  # 自動ステージ（試料ホルダー）の位置調整
+            if g_key == '6':stage_s.relative_move(2000, axis_num = 1, velocity = 9)                         # ６：右方向に1mm移動
+            elif g_key == '4':stage_s.relative_move(-2000, axis_num = 1, velocity = 9)                      # ４：左方向に1mm移動
+            elif g_key == '5':                                                                              # ５：ステージの初期位置に移動
                 if hi == 0 and vi == 0:
                     stage_s.move_origin()
                 else:
                     stage_s.biaxial_move(v = vi, vmode = 'a', h = hi, hmode = 'a')
-            elif g_key == '2':stage_s.relative_move(2000, axis_num = 2, velocity = 9)         # ２：上方向に1mm移動
-            elif g_key == '8':stage_s.relative_move(-2000, axis_num = 2, velocity = 9)        # ８：下方向に1mm移動
+            elif g_key == '2':stage_s.relative_move(2000, axis_num = 2, velocity = 9)                       # ２：上方向に1mm移動
+            elif g_key == '8':stage_s.relative_move(-2000, axis_num = 2, velocity = 9)                      # ８：下方向に1mm移動
             location[0] = stage_s.read_position(axis_num = 1)
             location[1] = stage_s.read_position(axis_num = 2)
             print('CRUX stage position : x={}[mm], y={}[mm], z={}[mm]'.format((location[0]-hi)/pl_rate,(location[1]-vi)/pl_rate,location[2]/pl_rate))
         
-        # ピエゾステージ（参照ミラー）の位置調整
-        if g_key in ['7','9','1','3','0']:
-            if g_key == '7':stage_m.relative_move(100)                                      # 7：前方に100nm移動
-            elif g_key == '9':stage_m.relative_move(10)                                     # 9：前方に10nm移動
-            elif g_key == '1':stage_m.relative_move(200)                                    # 1：前方に200nm移動
-            elif g_key == '3':stage_m.relative_move(20)                                     # 3：前方に20nm移動
-            elif g_key == '0':stage_m.absolute_move(0)                                      # 0：ステージの初期位置に移動
+        if g_key in ['7','9','1','3','0']:                  # ピエゾステージ（参照ミラー）の位置調整
+            if g_key == '7':stage_m.relative_move(100)                                                      # 7：前方に100nm移動
+            elif g_key == '9':stage_m.relative_move(10)                                                     # 9：前方に10nm移動
+            elif g_key == '1':stage_m.relative_move(200)                                                    # 1：前方に200nm移動
+            elif g_key == '3':stage_m.relative_move(20)                                                     # 3：前方に20nm移動
+            elif g_key == '0':stage_m.absolute_move(0)                                                      # 0：ステージの初期位置に移動
             print('FINE stage position : x={}[nm]'.format(stage_m.status['position']))
         
-        # '/' キーでステージを左端に移動する（サンプル変更時用）
-        if g_key=='/': 
+        if g_key == '/':                                    # '/' キーでステージを左端に移動する
             stage_s.absolute_move(-71000)
-
         
-        # <リアルタイム測定：CCS>（光強度が飽和しているとエラーメッセージ）
-        try: itf[0,:] = ccs.read_spectra(averaging = 5)
+
+        """ リアルタイム測定 """
+        try: itf[0,:] = ccs.read_spectra(averaging = 5)     # <CCS測定>（光強度が飽和しているとエラーメッセージ）
         except CcsError as ccs_e:
             ccs_err = True
             print(ccs_e, end = "\r")
@@ -213,19 +211,18 @@ if __name__ == "__main__":
                 print("                                     ", end = "\r")
                 ax0_0.set_color('tab:blue')
                 ccs_err = False
-        ax0_0.set_data(ccs.wavelength[ccs_st:ccs_ed], itf[0, ccs_st:ccs_ed])            # グラフのアップデート
+        ax0_0.set_data(ccs.wavelength[ccs_st:ccs_ed], itf[0, ccs_st:ccs_ed])
         ax0.set_ylim((0, 1.2*itf[0, ccs_st:ccs_ed].max()))
 
         if reference is not None:
-            ascan = sp.generate_ascan(itf[0,ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
+            ascan = sp.generate_ascan(itf[0, ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
             if use_um:
                 ax1_0.set_data(sp.depth*1e3, ascan)  
             else:
                 ax1_0.set_data(sp.depth, ascan)
             ax1.set_ylim((0,np.amax(ascan)))
 
-        # <リアルタイム測定：PMA>（光強度が飽和しているとエラーメッセージ）
-        try: reflect[0,:] = pma.read_spectra(averaging = 5)
+        try: reflect[0,:] = pma.read_spectra(averaging = 5) # <PMA測定>（光強度が飽和しているとエラーメッセージ）
         except PmaError as pma_e:
             pma_err = True
             print(pma_e, end = "\r")
@@ -235,12 +232,10 @@ if __name__ == "__main__":
                 print("                                     ", end = "\r")
                 ax2_0.set_color('tab:blue')
                 pma_err = False               
-        ax2_0.set_data(pma.wavelength[pma_st:pma_ed], reflect[0,pma_st:pma_ed])
+        ax2_0.set_data(pma.wavelength[pma_st:pma_ed], reflect[0, pma_st:pma_ed])
         
-        # 信号処理（反射率）
-        if incident is not None:
-            # reflectance = sp.calculate_absorbance(reflect[0, pma_st:pma_ed], incident[pma_st:pma_ed])
-            reflectance = sp.calculate_reflectance(reflect[0, pma_st:pma_ed], incident[pma_st:pma_ed])
+        if incident is not None:                            # 信号処理（リアルタイムでの反射率）
+            reflectance = calculate_reflectance(reflect[0, pma_st:pma_ed], incident[pma_st:pma_ed])
             ax3_0.set_data(pma.wavelength[pma_st:pma_ed], reflectance)
             ax3.set_ylim(0, np.nanmax(reflectance))
         
@@ -276,12 +271,13 @@ if __name__ == "__main__":
                     itf[i,:] = ccs.read_spectra(averaging)
                     stage_s.relative_move(int(width/step_h*pl_rate*(-1)))
                 stage_s.move_origin(axis_num = 1, ret_form = 1)
-                result_map=sp.generate_bscan(itf[:,ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
+                
+                result_map = sp.generate_bscan(itf[:,ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
                 plt.figure()
                 plt.imshow(result_map ,cmap = 'jet', extent = [0,depth_max,0,width], aspect = (depth_max/width)*(2/3), vmax = 0.5)
                 plt.colorbar()
-                plt.xlabel('depth[mm]')
-                plt.ylabel('width[mm]')
+                plt.xlabel('Depth [mm]')
+                plt.ylabel('Width [mm]')
                 dh.save_spectra(wavelength = ccs.wavelength, reference = reference, spectra = itf.T, memo = memo)
                 plt.show()
 
@@ -303,7 +299,7 @@ if __name__ == "__main__":
                     stage_s.biaxial_move(v = int(height/step_v*pl_rate*(-1)), vmode = 'r', h = int((width*pl_rate/2)), hmode = 'a')
                 dh.save_spectra_3d(wavelength = ccs.wavelength, width = width, height = height, reference = reference, spectra = itf_3d, memo = memo)
       
-        if g_key == 'a':                                    # 'a' キーでリファレンスとa-scanのデータを削除する  
+        if g_key == 'g':                                    # 'g' キーでリファレンスとa-scanのデータを削除する  
             reference = None
             ax0_1.set_data(ccs.wavelength[ccs_st:ccs_ed], np.zeros(ccs_ed - ccs_st))
             ax1_0.set_data(sp.depth*1e3, np.zeros_like(sp.depth))
@@ -318,12 +314,12 @@ if __name__ == "__main__":
             ax2_1.set_data(pma.wavelength[pma_st:pma_ed], incident[pma_st:pma_ed])
 
         if g_key == 'x':                                    # 'x' キーでリファレンスのデータを保存する
-            data=pma.read_spectra(averaging = 100)
+            data = pma.read_spectra(averaging = 100)
             if incident is None:
                 dh.save_spectra(wavelength = pma.wavelength, spectra = data)
                 print('Message:Incident light spectra data was not registered. Only spectra data was saved.')
             else:
-                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = data, memo = 'Attention:This is absorbance measurement data.')
+                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = data, memo = 'Attention:This is reflectance measurement data.')
         
         if g_key =='c' and stage_s_flag:                    # 'c' キーで測定開始（2次元のデータ）
             if incident is None or stage_s_flag == False:
@@ -339,13 +335,10 @@ if __name__ == "__main__":
                     stage_s.relative_move(int(width/step_h*pl_rate*(-1)))
                 stage_s.move_origin(axis_num = 1, ret_form = 1)
 
-                dh.save_spectra(wavelength=pma.wavelength,reference=incident,spectra=reflect.T,memo='Attention:This is absorbance measurement data.'+memo)
-
-                #signal processing and plot
-                # result_map=sp.calculate_absorbance_2d(reflection=reflect[:,pma_st:pma_ed])
-                result_map = sp.calculate_reflectance_2d(reflect[:,pma_st:pma_ed], incident[pma_st:pma_ed])
+                result_map = calculate_reflectance_2d(reflect[:,pma_st:pma_ed], incident[pma_st:pma_ed])
+                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = reflect.T, memo = 'Attention:This is reflectance measurement data.'+memo)
                 plt.figure()
-                plt.imshow(result_map,cmap='jet', extent=[pma.wavelength[pma_st], pma.wavelength[pma_ed],0,width], aspect=((abs(pma.wavelength[pma_st]-pma.wavelength[pma_ed])/width))*(2/3),)
+                plt.imshow(result_map, cmap = 'jet', extent = [pma.wavelength[pma_st],pma.wavelength[pma_ed],0,width], aspect = ((abs(pma.wavelength[pma_st]-pma.wavelength[pma_ed])/width))*(2/3))
                 plt.colorbar()
                 plt.xlabel('Wavelength [nm]')
                 plt.ylabel('Width [mm]')
@@ -366,9 +359,9 @@ if __name__ == "__main__":
                         reflect_3d[i][j] = pma.read_spectra(averaging = averaging)
                         stage_s.relative_move(int(width/step_h*pl_rate*(-1)))
                     stage_s.biaxial_move(v = int(height/step_v*pl_rate*(-1)), vmode = 'r', h = int((width*pl_rate/2)), hmode = 'a')
-                dh.save_spectra_3d(wavelength=pma.wavelength, width=width, height=height, reference=incident, spectra=reflect, memo=memo+'Attention:This is absorbance measurement data.')
+                dh.save_spectra_3d(wavelength = pma.wavelength, width = width, height = height, reference = incident, spectra = reflect, memo = memo+'Attention:This is reflectance measurement data.')
 
-        if g_key == 'z':                                    # 'z' キーでリファレンスのデータを削除する
+        if g_key == 'b':                                    # 'b' キーでリファレンスのデータを削除する
             incident = None
             ax2_1.set_data(pma.wavelength[pma_st:pma_ed], np.zeros(pma_ed-pma_st)+1)
             ax3_0.set_data(pma.wavelength[pma_st:pma_ed], np.zeros(pma_ed-pma_st))
@@ -396,12 +389,12 @@ if __name__ == "__main__":
             file_path = dh.generate_filename('png')
             plt.savefig(file_path)
             print("Saved the graph to {}.".format(file_path))
-            data=pma.read_spectra(averaging = 100)
+            data = pma.read_spectra(averaging = 100)
             if incident is None:
                 dh.save_spectra(wavelength = pma.wavelength, spectra = data)
                 print('Message:Incident light spectra data was not registered. Only spectra data was saved.')
             else:
-                dh.save_spectra(wavelength=pma.wavelength, reference=incident, spectra=data, memo='Attention:This is absorbance measurement data.')
+                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = data, memo = 'Attention:This is reflectance measurement data.')
 
         if g_key == 'e' and stage_s_flag:                   # 'e' キーで測定開始（2次元のデータ）
             if incident is None or reference is None or stage_s_flag == False:
@@ -415,7 +408,7 @@ if __name__ == "__main__":
             else:
                 print('OCT & REF:Measurement(2D) start')
 
-                # OCT+反射率の測定ループ
+                # OCTと反射率の同時測定ループ
                 stage_s.absolute_move(int((width*pl_rate/2)+hi))
                 for i in tqdm(range(step_h)):
                     reflect[i,:] = pma.read_spectra(averaging = averaging)
@@ -423,13 +416,10 @@ if __name__ == "__main__":
                     stage_s.relative_move(int(width/step_h*pl_rate*(-1)))
                 stage_s.biaxial_move(v = vi, vmode = 'a', h = hi, hmode = 'a')
 
-                # 信号処理
-                result_abs = sp.calculate_absorbance_2d(reflection = reflect)
+                result_abs = calculate_reflectance_2d(reflection = reflect)
                 result_oct = sp.generate_bscan(itf[:,ccs_st:ccs_ed], reference[ccs_st:ccs_ed])
-                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = reflect.T, memo = 'Attention:This is absorbance measurement data.')
+                dh.save_spectra(wavelength = pma.wavelength, reference = incident, spectra = reflect.T, memo = 'Attention:This is reflectance measurement data.')
                 dh.save_spectra(wavelength = ccs.wavelength, reference = reference, spectra = itf.T)
-
-                # グラフ出力
                 plt.figure()
                 plt.imshow(result_map, cmap = 'jet', extent = [0,depth_max,0,width], aspect = (depth_max/width)*(2/3), vmax = 0.5)
                 plt.colorbar()
@@ -449,20 +439,20 @@ if __name__ == "__main__":
             else:
                 print('REF:Measurement(3D) start')
 
-                # OCT+反射率の測定ループ
+                # OCTと反射率の同時測定ループ
                 reflect_3d = np.zeros((step_v, step_h, pma.wavelength), dtype = float)
                 itf_3d = np.zeros((step_v, step_h, ccs.wavelength.size), dtype = float)
-                stage_s.biaxial_move(v=int(height*pl_rate/2)+vi, vmode='a', h=int((width*pl_rate/2))+hi, hmode='a')
+                stage_s.biaxial_move(v=int(height*pl_rate/2)+vi, vmode = 'a', h = int((width*pl_rate/2))+hi, hmode = 'a')
                 for i in tqdm (range(step_v)):
                     for j in range(step_h):
                         reflect_3d[i][j] = pma.read_spectra(averaging = averaging)
                         itf_3d[i][j] = ccs.read_spectra(averaging)
                         stage_s.relative_move(int(width/step_h*pl_rate*(-1)))
-                    stage_s.biaxial_move(v=int(height/step_v*pl_rate*(-1)), vmode='r', h=int((width*pl_rate/2)), hmode='a')
-                stage_s.biaxial_move(v=vi, vmode='a', h=hi, hmode='a')
+                    stage_s.biaxial_move(v=int(height/step_v*pl_rate*(-1)), vmode = 'r', h = int((width*pl_rate/2)), hmode = 'a')
+                stage_s.biaxial_move(v = vi, vmode = 'a', h = hi, hmode = 'a')
 
-                dh.save_spectra_3d(wavelength=ccs.wavelength, width=width, height=height, reference=reference, spectra=itf_3d, memo=memo)
-                dh.save_spectra_3d(wavelength=pma.wavelength, width=width, height=height, reference=incident, spectra=reflect, memo=memo+'Attention:This is absorbance measurement data.')
+                dh.save_spectra_3d(wavelength = ccs.wavelength, width = width, height = height, reference = reference, spectra = itf_3d, memo = memo)
+                dh.save_spectra_3d(wavelength = pma.wavelength, width = width, height = height, reference = incident, spectra = reflect, memo = memo+'Attention:This is reflectance measurement data.')
         
         if g_key == 't':                                    # 't' キーでOCTとSSのリファレンスデータを削除する
             reference = None
